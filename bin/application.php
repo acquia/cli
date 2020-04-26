e2af9abe4fedd67fb32d0f59606f5f6fefdb2250
Matthew Grasmick <matt.grasmick@gmail.com>
2020-04-26 12:03:11 -0400

    simplify bootstrap.

--- bin/application.php
+++ bin/application.php
@@ -28,6 +28,13 @@ use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
+$pharPath = Phar::running(true);
+if ($pharPath) {
+    require_once "$pharPath/vendor/autoload.php";
+} else {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
 // Create the input and output objects for ads to run against.
 $input = new ArgvInput($_SERVER['argv']);
 $output = new ConsoleOutput();
@@ -66,3 +73,83 @@ $application->addCommands([
 
 $status_code = $application->run($input, $output);
 exit($status_code);
+
+/**
+ * Finds the root directory for the repository.
+ *
+ * @return null|string
+ *   Root.
+ */
+function find_repo_root()
+{
+    $possible_repo_roots = [
+      getcwd(),
+    ];
+    // Check for PWD - some local environments will not have this key.
+    if (isset($_SERVER['PWD']) && !in_array($_SERVER['PWD'], $possible_repo_roots, true)) {
+        array_unshift($possible_repo_roots, $_SERVER['PWD']);
+    }
+    foreach ($possible_repo_roots as $possible_repo_root) {
+        if ($repo_root = find_directory_containing_files($possible_repo_root, ['docroot', 'vendor/autoload.php'])) {
+            return realpath($repo_root);
+        }
+    }
+
+    return null;
+}
+
+/**
+ * Traverses file system upwards in search of a given file.
+ *
+ * Begins searching for $file in $working_directory and climbs up directories
+ * $max_height times, repeating search.
+ *
+ * @param string $working_directory
+ *   Working directory.
+ * @param array $files
+ *   Files.
+ * @param int $max_height
+ *   Max Height.
+ *
+ * @return bool|string
+ *   FALSE if file was not found. Otherwise, the directory path containing the
+ *   file.
+ */
+function find_directory_containing_files($working_directory, array $files, $max_height = 10)
+{
+    // Find the root directory of the git repository containing BLT.
+    // We traverse the file tree upwards $max_height times until we find
+    // vendor/bin/blt.
+    $file_path = $working_directory;
+    for ($i = 0; $i <= $max_height; $i++) {
+        if (files_exist($file_path, $files)) {
+            return $file_path;
+        }
+
+        $file_path = dirname($file_path) . '';
+    }
+
+    return false;
+}
+
+/**
+ * Determines if an array of files exist in a particular directory.
+ *
+ * @param string $dir
+ *   Dir.
+ * @param array $files
+ *   Files.
+ *
+ * @return bool
+ *   Exists.
+ */
+function files_exist($dir, array $files)
+{
+    foreach ($files as $file) {
+        if (file_exists($dir . '/' . $file)) {
+            return true;
+        }
+    }
+
+    return false;
+}
