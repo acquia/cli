a4a58c515009f3c80eda056cc2afbf1a2fadf52c
Matthew Grasmick <matt.grasmick@gmail.com>
2020-05-10 21:32:20 -0400

    Adding test for unlink command.

--- src/Command/UnlinkCommand.php
+++ src/Command/UnlinkCommand.php
@@ -36,21 +36,24 @@ class UnlinkCommand extends CommandBase {
   protected function execute(InputInterface $input, OutputInterface $output) {
     $this->validateCwdIsValidDrupalProject();
 
-    $local_user_config = $this->getDatastore()->get('acquia-cli/user.json');
     $repo_root = $this->getApplication()->getRepoRoot();
+    $local_user_config = $this->getDatastore()->get($this->getApplication()->getAcliConfigFilename());
+    if ($local_user_config === NULL || !array_key_exists('localProjects', $local_user_config)) {
+      throw new AcquiaCliException('There is no Acquia Cloud application linked to {repo_root}', ['repo_root' => $repo_root]);
+    }
     foreach ($local_user_config['localProjects'] as $key => $project) {
       if ($project['directory'] === $repo_root) {
         // @todo Add confirmation.
         unset($local_user_config['localProjects'][$key]);
         $this->localProjectInfo = NULL;
-        $this->getDatastore()->set('acquia-cli/user.json', $local_user_config);
+        $this->getDatastore()->set($this->getApplication()->getAcliConfigFilename(), $local_user_config);
 
         $output->writeln("<info>Unlinked $repo_root from Cloud application {$project['cloud_application_uuid']}</info>");
         return 0;
       }
     }
 
-    throw new AcquiaCliException("This project is not linked to a Cloud application.");
+    throw new AcquiaCliException('This project is not linked to a Cloud application.');
   }
 
 }
