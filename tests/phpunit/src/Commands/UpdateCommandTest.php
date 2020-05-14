c2e37cba8b7e71aed009f01b871441df500faede
Matthew Grasmick <matt.grasmick@gmail.com>
2020-05-13 21:04:27 -0400

    Adding library for update command. (#38)

--- tests/phpunit/src/Commands/UpdateCommandTest.php
+++ tests/phpunit/src/Commands/UpdateCommandTest.php
@@ -1,5 +1,34 @@
 <?php
 
+namespace Acquia\Cli\Tests\Commands;
+
+use Acquia\Cli\Command\UnlinkCommand;
+use Acquia\Cli\Command\UpdateCommand;
+use Acquia\Cli\Tests\CommandTestBase;
+use Symfony\Component\Console\Command\Command;
+
 /**
- * @file
+ * Class UpdateCommandTest.
+ *
+ * @property \Acquia\Cli\Command\UpdateCommand $command
+ * @package Acquia\Cli\Tests\Commands
  */
+class UpdateCommandTest extends CommandTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createCommand(): Command {
+    return new UpdateCommand();
+  }
+
+  public function testNonPharException() {
+    try {
+      $this->executeCommand([], []);
+    }
+    catch (\Exception $e) {
+      $this->assertStringContainsString('update only works when running the phar version of ', $e->getMessage());
+    }
+  }
+
+}
