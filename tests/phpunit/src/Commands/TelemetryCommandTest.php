06f6d41614c77b783f12bad80e258465071e02ac
Dane Powell <git@danepowell.com>
2020-05-14 09:55:59 -0700

    Added unit test.

--- tests/phpunit/src/Commands/TelemetryCommandTest.php
+++ tests/phpunit/src/Commands/TelemetryCommandTest.php
@@ -1,5 +1,36 @@
 <?php
 
+namespace Acquia\Cli\Tests\Commands;
+
+use Acquia\Cli\Command\TelemetryCommand;
+use Acquia\Cli\Tests\CommandTestBase;
+use Symfony\Component\Console\Command\Command;
+
 /**
- * @file
+ * Class TelemetryCommandTest.
+ *
+ * @property \Acquia\Cli\Command\TelemetryCommand $command
+ * @package Acquia\Cli\Tests\Commands
  */
+class TelemetryCommandTest extends CommandTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createCommand(): Command {
+    return new TelemetryCommand();
+  }
+
+  /**
+   * Tests the 'telemetry' command.
+   */
+  public function testTelemetryCommand(): void {
+    $this->executeCommand();
+    $output = $this->getDisplay();
+    $this->assertStringContainsString('Telemetry has been enabled.', $output);
+    $this->executeCommand();
+    $output = $this->getDisplay();
+    $this->assertStringContainsString('Telemetry has been disabled.', $output);
+  }
+
+}
