70595d88b100cdc49d238233e2ac501f9b5cea23
Matthew Grasmick <matt.grasmick@gmail.com>
2020-05-16 15:50:39 -0400

    Add startBrowserTest.

---
+++ tests/phpunit/src/LocalMachineHelperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Acquia\Cli\Tests;
+
+/**
+ * Class LocalMachineHelperTest.
+ */
+class LocalMachineHelperTest extends TestBase {
+
+  public function setUp($output = NULL): void {
+    parent::setUp();
+    putenv('DISPLAY=1');
+  }
+
+  public function tearDown(): void {
+    parent::tearDown();
+    putenv('DISPLAY');
+  }
+
+  public function testStartBrowser() {
+    $local_machine_helper = $this->application->getLocalMachineHelper();
+    $opened = $local_machine_helper->startBrowser('https://google.com');
+    $this->assertTrue($opened, 'Failed to open browser');
+  }
+
+}
