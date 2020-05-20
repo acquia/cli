b634df7e0a1e0b5afe063669f55ddeb970ad508b
Dane Powell <git@danepowell.com>
2020-05-20 11:27:21 -0700

    Use prophecy instead.

--- tests/phpunit/src/AcquiaCliApplicationTest.php
+++ tests/phpunit/src/AcquiaCliApplicationTest.php
@@ -2,16 +2,17 @@
 
 namespace Acquia\Cli\Tests;
 
+use Prophecy\Argument;
 use Zumba\Amplitude\Amplitude;
 
 class AcquiaCliApplicationTest extends TestBase {
 
   public function testRun(): void {
-    $this->application->amplitude = $this->getMockBuilder(Amplitude::class)
-      ->onlyMethods(['queueEvent'])
-      ->getMock();
-    $this->application->amplitude->expects($this->exactly(1))->method('queueEvent');
+    $amplitude = $this->prophet->prophesize(Amplitude::class);
+    $this->application->amplitude = $amplitude->reveal();
+    $amplitude->queueEvent('Ran command', Argument::type('array'))->shouldBeCalled();
     $exit_code = $this->application->run($this->input, $this->consoleOutput);
     $this->assertEquals(0, $exit_code);
+    $this->prophet->checkPredictions();
   }
 }
