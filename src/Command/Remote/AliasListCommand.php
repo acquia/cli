a1fce5b4e099543899afbac1254c99a3eaf65936
Matthew Grasmick <matt.grasmick@gmail.com>
2020-05-27 23:21:14 -0400

    Change remote:aliases-list to list only for a single app.

--- src/Command/Remote/AliasListCommand.php
+++ src/Command/Remote/AliasListCommand.php
@@ -5,7 +5,6 @@ namespace Acquia\Cli\Command\Remote;
 use Acquia\Cli\Command\CommandBase;
 use AcquiaCloudApi\Endpoints\Applications;
 use AcquiaCloudApi\Endpoints\Environments;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -32,35 +31,22 @@ class AliasListCommand extends CommandBase {
   protected function execute(InputInterface $input, OutputInterface $output) {
     $acquia_cloud_client = $this->getApplication()->getAcquiaCloudClient();
     $applications_resource = new Applications($acquia_cloud_client);
-    if ($cloud_application_uuid = $this->determineCloudApplication()) {
-      $customer_applications = [$applications_resource->get($cloud_application_uuid)];
-    }
-    else {
-      $customer_applications = $applications_resource->getAll();
-    }
+    $cloud_application_uuid = $this->determineCloudApplication();
+    $customer_application = $applications_resource->get($cloud_application_uuid);
     $environments_resource = new Environments($acquia_cloud_client);
 
     $table = new Table($this->output);
     $table->setHeaders(['Environment Alias', 'Application', 'Environment UUID']);
 
-    $count = count($customer_applications);
-    $progressBar = new ProgressBar($output, $count);
-    $progressBar->setFormat('message');
-    $progressBar->setMessage("Fetching aliases for <comment>$count applications</comment> from Acquia Cloud...");
-    $progressBar->start();
-    foreach ($customer_applications as $customer_application) {
-      $progressBar->setMessage("Fetching aliases for <comment>{$customer_application->name}</comment>");
-      $site_id = $customer_application->hosting->id;
-      $parts = explode(':', $site_id);
-      $site_prefix = $parts[1];
-      $environments = $environments_resource->getAll($customer_application->uuid);
-      foreach ($environments as $environment) {
-        $alias = $site_prefix . '.' . $environment->name;
-        $table->addRow([$customer_application->name, $alias, $environment->uuid]);
-      }
-      $progressBar->advance();
+    $site_id = $customer_application->hosting->id;
+    $parts = explode(':', $site_id);
+    $site_prefix = $parts[1];
+    $environments = $environments_resource->getAll($customer_application->uuid);
+    foreach ($environments as $environment) {
+      $alias = $site_prefix . '.' . $environment->name;
+      $table->addRow([$customer_application->name, $alias, $environment->uuid]);
     }
-    $progressBar->finish();
+
     $table->render();
 
     return 0;
