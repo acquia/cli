2b64bcf231d86bb5224b08cbd1dee840f5254281
Matthew Grasmick <matt.grasmick@gmail.com>
2020-09-15 11:44:43 -0400

    Add --regenerate option.

--- src/Command/Ide/IdeShareCommand.php
+++ src/Command/Ide/IdeShareCommand.php
@@ -5,8 +5,6 @@ namespace Acquia\Cli\Command\Ide;
 use Acquia\Cli\Command\CommandBase;
 use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
 use AcquiaCloudApi\Endpoints\Ides;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -28,6 +26,7 @@ class IdeShareCommand extends CommandBase {
    */
   protected function configure() {
     $this->setDescription('Get the share URL for a Cloud IDE')
+      ->addOption('regenerate', '', InputOption::VALUE_NONE, 'regenerate the share code')
       ->setHidden(!AcquiaDrupalEnvironmentDetector::isAhIdeEnv());
   }
 
@@ -40,10 +39,15 @@ class IdeShareCommand extends CommandBase {
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
     $this->requireCloudIdeEnvironment();
+
+    if ($input->getOption('regenerate')) {
+      $this->regenerateShareCode();
+    }
+
+    $share_uuid = $this->localMachineHelper->readFile($this->getShareCodeFilepath());
     $acquia_cloud_client = $this->cloudApiClientService->getClient();
     $ides_resource = new Ides($acquia_cloud_client);
     $ide = $ides_resource->get($this::getThisCloudIdeUuid());
-    $share_uuid = $this->localMachineHelper->readFile($this->getShareCodeFilepath());
 
     $this->output->writeln('');
     $this->output->writeln("<comment>Your IDE Share URL:</comment> <href={$ide->links->ide->href}>{$ide->links->ide->href}?share=$share_uuid</>");
@@ -66,6 +70,13 @@ class IdeShareCommand extends CommandBase {
       $this->shareCodeFilepath = '/usr/local/share/ide/.sharecode';
     }
     return $this->shareCodeFilepath;
+  }
+
+  /**
+   * @throws \Exception
+   */
+  public function regenerateShareCode(): void {
+    $this->localMachineHelper->writeFile($this->getShareCodeFilepath(), md5(random_bytes(10)));
   }
 
 }
