7504ad28ea7f1e28f1a1767d17c5c6fa45bf6730
Dane Powell <git@danepowell.com>
2020-09-24 09:04:05 -0700

    DX-2334: Indicate when errors come from Cloud API (#279)

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,6 +3,7 @@
 namespace Acquia\Cli\EventListener;
 
 use Acquia\Cli\Exception\AcquiaCliException;
+use AcquiaCloudApi\Exception\ApiErrorException;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Component\Console\Event\ConsoleErrorEvent;
 
@@ -16,6 +17,10 @@ class ExceptionListener
     if ($error instanceof IdentityProviderException && $error->getMessage() === 'invalid_client') {
       $event->setError(new AcquiaCliException('Your Cloud API credentials are invalid. Run acli auth:login to reset them.',
         [], $exitCode));
+    }
+
+    if ($error instanceof ApiErrorException) {
+      $event->setError(new AcquiaCliException('Acquia Cloud Platform API returned an error: ' . $error->getMessage(), [], $exitCode));
     }
   }
 
