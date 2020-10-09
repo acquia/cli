71239be8d8fe4806546db788c5d01188244a6458
Matthew Grasmick <matt.grasmick@gmail.com>
2020-10-09 15:14:38 -0400

    fix test.

--- tests/phpunit/src/Commands/Remote/DrushCommandTest.php
+++ tests/phpunit/src/Commands/Remote/DrushCommandTest.php
@@ -64,7 +64,7 @@ class DrushCommandTest extends SshCommandTestBase {
       'status --fields=db-status',
     ];
     $local_machine_helper
-      ->execute($ssh_command, Argument::type('callable'), NULL, TRUE, 600)
+      ->execute($ssh_command, Argument::type('callable'), NULL, TRUE, NULL)
       ->willReturn($process->reveal())
       ->shouldBeCalled();
     $this->command->localMachineHelper = $local_machine_helper->reveal();
