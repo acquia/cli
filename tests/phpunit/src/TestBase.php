b92757f3721e6bbbfdab03072de86cd054d5c1a5
Matthew Grasmick <matt.grasmick@gmail.com>
2020-10-19 12:22:15 -0400

    Remove more.

--- tests/phpunit/src/TestBase.php
+++ tests/phpunit/src/TestBase.php
@@ -199,9 +199,6 @@ abstract class TestBase extends TestCase {
     $this->clientProphecy = $this->prophet->prophesize(Client::class);
     $this->clientProphecy->addOption('headers', ['User-Agent' => 'acli/UNKNOWN', 'Accept' => 'application/json']);
     $this->localMachineHelper = new LocalMachineHelper($this->input, $output, $this->logger);
-    $this->updateHelper = new UpdateHelper();
-    $guzzle_client = $this->mockGuzzleClientForUpdate(UpdateCommandTest::mockGitHubReleasesResponse());
-    $this->updateHelper->setClient($guzzle_client->reveal());
     $this->clientServiceProphecy = $this->prophet->prophesize(ClientService::class);
     $this->clientServiceProphecy->getClient()->willReturn($this->clientProphecy->reveal());
     $this->telemetryHelper = new TelemetryHelper($this->input, $output, $this->clientServiceProphecy->reveal(), $this->acliDatastore, $this->cloudDatastore);
