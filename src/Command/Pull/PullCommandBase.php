4b19b4c88bd7f5da3dc17fbdd8b35d19f57100bb
Dane Powell <git@danepowell.com>
2020-10-22 18:59:19 -0700

    fix db name

--- src/Command/Pull/PullCommandBase.php
+++ src/Command/Pull/PullCommandBase.php
@@ -89,11 +89,9 @@ abstract class PullCommandBase extends CommandBase {
     $this->checklist->addItem('Importing Drupal database copy from the Cloud Platform');
     $this->importRemoteDatabase($source_environment, $database, $this->getOutputCallback($output, $this->checklist));
     $sitegroup = self::getSiteGroupFromSshUrl($source_environment);
-    $db_name = $this->getNameFromDatabaseResponse($database);
     $default_path = "/var/www/site-php/$sitegroup/$sitegroup-settings.inc";
-    $db_name_path = "/var/www/site-php/$sitegroup/$db_name-settings.inc";
+    $db_name_path = "/var/www/site-php/$sitegroup/{$database->name}-settings.inc";
     $this->localMachineHelper->execute(['cp', $default_path, $db_name_path]);
-
     $this->checklist->completePreviousItem();
   }
 
