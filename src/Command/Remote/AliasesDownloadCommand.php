8c061fd6578c61372b2ce3780d99ad9339d4db49
Dane Powell <git@danepowell.com>
2020-10-29 13:42:02 -0700

    Fix #300: Download Drush 9 aliases

--- src/Command/Remote/AliasesDownloadCommand.php
+++ src/Command/Remote/AliasesDownloadCommand.php
@@ -40,6 +40,7 @@ class AliasesDownloadCommand extends SshCommand {
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
     $acquia_cloud_client = $this->cloudApiClientService->getClient();
+    $acquia_cloud_client->addQuery('version', 9);
     $account_adapter = new Account($acquia_cloud_client);
     $aliases = $account_adapter->getDrushAliases();
     $drush_archive_filepath = $this->getDrushArchiveTempFilepath();
@@ -49,22 +50,25 @@ class AliasesDownloadCommand extends SshCommand {
     $this->output->writeln(sprintf(
       'Cloud Platform Drush Aliases archive downloaded to <options=bold>%s</>',
       $drush_archive_filepath
-    ));
+    ), OutputInterface::VERBOSITY_VERBOSE);
 
     $this->localMachineHelper->getFilesystem()->mkdir($drush_aliases_dir);
     $this->localMachineHelper->getFilesystem()->chmod($drush_aliases_dir, 0700);
 
-    // @todo Upgrade to Drush 9 aliases and the open from sites subdir, not .drush!
-    $archive = new PharData($drush_archive_filepath . '/.drush');
+    // Tarball may have many subdirectories, which one to extract?
+    $base_dir = 'sites';
+    $archive = new PharData($drush_archive_filepath . '/' . $base_dir);
     $drushFiles = [];
     foreach (new RecursiveIteratorIterator($archive, RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
-      $drushFiles[] = '.drush/' . $file->getFileName();
+      $drushFiles[] = $base_dir . '/' . $file->getFileName();
     }
     $archive->extractTo(dirname($drush_aliases_dir), $drushFiles, TRUE);
     $this->output->writeln(sprintf(
       'Cloud Platform Drush aliases installed into <options=bold>%s</>',
       $drush_aliases_dir
     ));
+    $this->output->writeln('Drush 9+ does not automatically read aliases from this directory. Run <comment>drush core:init</comment> to ensure these aliases are discovered.');
+    $this->output->writeln('For more details, see https://github.com/drush-ops/drush/blob/master/examples/example.site.yml');
     unlink($drush_archive_filepath);
 
     return 0;
@@ -96,7 +100,7 @@ class AliasesDownloadCommand extends SshCommand {
   protected function getDrushAliasesDir(): string {
     if (!isset($this->drushAliasesDir)) {
       $this->drushAliasesDir = $this->localMachineHelper
-          ->getLocalFilepath('~') . '/.drush';
+          ->getLocalFilepath('~') . '/.drush/sites';
     }
 
     return $this->drushAliasesDir;
