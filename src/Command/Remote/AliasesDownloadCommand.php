7b0d3850cb204402468b04c373c3074cd606989e
Dane Powell <git@danepowell.com>
2020-10-30 10:40:27 -0700

    Support both Drush 8 and 9

--- src/Command/Remote/AliasesDownloadCommand.php
+++ src/Command/Remote/AliasesDownloadCommand.php
@@ -40,12 +40,13 @@ class AliasesDownloadCommand extends SshCommand {
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
     $acquia_cloud_client = $this->cloudApiClientService->getClient();
-    $acquia_cloud_client->addQuery('version', 9);
+    $alias_version = $this->promptChooseDrushAliasVersion();
+    $acquia_cloud_client->addQuery('version', $alias_version);
     $account_adapter = new Account($acquia_cloud_client);
     $aliases = $account_adapter->getDrushAliases();
     $drush_archive_filepath = $this->getDrushArchiveTempFilepath();
     $this->localMachineHelper->writeFile($drush_archive_filepath, $aliases);
-    $drush_aliases_dir = $this->getDrushAliasesDir();
+    $drush_aliases_dir = $this->getDrushAliasesDir($alias_version);
 
     // This message is useful for debugging but could be misleading in ordinary
     // usage, because the archive is deleted before the command exits.
@@ -58,7 +59,7 @@ class AliasesDownloadCommand extends SshCommand {
     $this->localMachineHelper->getFilesystem()->chmod($drush_aliases_dir, 0700);
 
     // Tarball may have many subdirectories, only extract this one.
-    $base_dir = 'sites';
+    $base_dir = $alias_version == 8 ? '.drush' : 'sites';
     $archive = new PharData($drush_archive_filepath . '/' . $base_dir);
     $drushFiles = [];
     foreach (new RecursiveIteratorIterator($archive, RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
@@ -69,11 +70,26 @@ class AliasesDownloadCommand extends SshCommand {
       'Cloud Platform Drush aliases installed into <options=bold>%s</>',
       $drush_aliases_dir
     ));
-    $this->output->writeln('Drush 9+ does not automatically read aliases from this directory. Run <comment>drush core:init</comment> to ensure these aliases are discovered.');
-    $this->output->writeln('For more details, see https://github.com/drush-ops/drush/blob/master/examples/example.site.yml');
+    if ($alias_version == 9) {
+      $this->output->writeln('Drush 9+ does not automatically read aliases from this directory. Run <comment>drush core:init</comment> to ensure these aliases are discovered.');
+      $this->output->writeln('For more details, see https://github.com/drush-ops/drush/blob/master/examples/example.site.yml');
+    }
     unlink($drush_archive_filepath);
 
     return 0;
+  }
+
+  /**
+   * Prompts the user for their preferred Drush alias version.
+   */
+  protected function promptChooseDrushAliasVersion() {
+    $this->io->writeln('Drush changed how aliases are defined in Drush 9. Drush 8 aliases are PHP-based, while Drush 9+ aliases are YAML-based.');
+    $question = 'Choose your preferred alias compatibility:';
+    $choices = [
+      8 => 'Drush 8 / Drupal 7 (PHP)',
+      9 => 'Drush 9+ / Drupal 8+ (YAML)',
+    ];
+    return array_search($this->io->choice($question, $choices, '9'), $choices);
   }
 
   /**
@@ -99,10 +115,13 @@ class AliasesDownloadCommand extends SshCommand {
    * @return string
    * @throws \Exception
    */
-  protected function getDrushAliasesDir(): string {
+  protected function getDrushAliasesDir($version): string {
     if (!isset($this->drushAliasesDir)) {
       $this->drushAliasesDir = $this->localMachineHelper
-          ->getLocalFilepath('~') . '/.drush/sites';
+          ->getLocalFilepath('~') . '/.drush';
+      if ($version == 9) {
+        $this->drushAliasesDir .= '/sites';
+      }
     }
 
     return $this->drushAliasesDir;
--- tests/phpunit/src/Commands/Remote/AliasesDownloadCommandTest.php
+++ tests/phpunit/src/Commands/Remote/AliasesDownloadCommandTest.php
@@ -26,11 +26,22 @@ class AliasesDownloadCommandTest extends CommandTestBase {
   }
 
   /**
+   * Test all Drush alias versions.
+   */
+  public function providerTestRemoteAliasesDownloadCommand(): array {
+    return [[8, 9]];
+  }
+
+  /**
+   * Tests the 'auth:login' command.
+   *
+   * @dataProvider providerTestRemoteAliasesDownloadCommand
+   *
    * Tests the 'remote:aliases:download' commands.
+   *
    * @throws \Exception
    */
-  public function testRemoteAliasesDownloadCommand(): void {
-
+  public function testRemoteAliasesDownloadCommand($alias_version): void {
     $drush_aliases_fixture = Path::canonicalize(__DIR__ . '/../../../../fixtures/drush-aliases');
     $drush_aliases_tarball_fixture_filepath = tempnam(sys_get_temp_dir(), 'AcquiaDrushAliases');
     $archive_fixture = new PharData($drush_aliases_tarball_fixture_filepath . '.tar');
@@ -38,13 +49,16 @@ class AliasesDownloadCommandTest extends CommandTestBase {
     $archive_fixture->compress(Phar::GZ);
 
     $stream = stream_for(file_get_contents($drush_aliases_tarball_fixture_filepath . '.tar.gz'));
-    $this->clientProphecy->addQuery('version', 9);
+    $this->clientProphecy->addQuery('version', $alias_version);
     $this->clientProphecy->request('get', '/account/drush-aliases/download')->willReturn($stream);
     $drush_archive_filepath = $this->command->getDrushArchiveTempFilepath();
-    $drush_aliases_dir = Path::join(sys_get_temp_dir(), '.drush/sites');
+    $drush_aliases_dir = Path::join(sys_get_temp_dir(), '.drush');
+    if ($alias_version == 9) {
+      $drush_aliases_dir = Path::join($drush_aliases_dir, 'sites');
+    }
     $this->command->setDrushAliasesDir($drush_aliases_dir);
 
-    $inputs = [];
+    $inputs = [$alias_version];
     $this->executeCommand([], $inputs);
 
     // Assert.
@@ -53,7 +67,6 @@ class AliasesDownloadCommandTest extends CommandTestBase {
 
     $this->assertFileDoesNotExist($drush_archive_filepath);
     $this->assertFileExists($drush_aliases_dir);
-    $this->assertStringContainsString("Cloud Platform Drush Aliases archive downloaded to $drush_archive_filepath", $output);
     $this->assertStringContainsString('Cloud Platform Drush aliases installed into ' . $drush_aliases_dir, $output);
 
   }
