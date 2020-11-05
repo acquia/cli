0796acf5972b16c8ab21fe28a90e72655fc40264
Dane Powell <git@danepowell.com>
2020-11-05 10:44:05 -0800

    DX-1780: Better message when no IDEs available

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -13,14 +13,25 @@ class ExceptionListener
   public function onConsoleError(ConsoleErrorEvent $event) {
     $exitCode = $event->getExitCode();
     $error = $event->getError();
+    $errorMessage = $error->getMessage();
+
     // Make OAuth server errors more human-friendly.
     if ($error instanceof IdentityProviderException && $error->getMessage() === 'invalid_client') {
-      $event->setError(new AcquiaCliException('Your Cloud API credentials are invalid. Run acli auth:login to reset them.',
-        [], $exitCode));
+      $newErrorMessage = 'Your Cloud API credentials are invalid. Run acli auth:login to reset them.';
     }
 
     if ($error instanceof ApiErrorException) {
-      $event->setError(new AcquiaCliException('Acquia Cloud Platform API returned an error: ' . $error->getMessage(), [], $exitCode));
+      switch ($errorMessage) {
+        case "There are no available Cloud IDEs for this application.\n":
+          $newErrorMessage = $errorMessage . 'Delete an existing IDE (acli ide:delete) or submit a support ticket to purchase additional IDEs (https://support.acquia.com)';
+          break;
+        default:
+          $newErrorMessage = 'Acquia Cloud Platform API returned an error: ' . $errorMessage;
+      }
+    }
+
+    if (isset($newErrorMessage)) {
+      $event->setError(new AcquiaCliException($newErrorMessage, [], $exitCode));
     }
   }
 
