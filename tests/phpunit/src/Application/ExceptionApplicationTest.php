f5c79250b69d79559200debc2c05c269070c5316
Dane Powell <git@danepowell.com>
2020-11-24 18:44:44 -0800

    restore tests

---
+++ tests/phpunit/src/Application/ExceptionApplicationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Acquia\Cli\Tests\Application;
+
+use Acquia\Cli\Kernel;
+use Acquia\Cli\Tests\TestBase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+/**
+ * Tests exceptions rewritten by the Symfony Event Dispatcher.
+ *
+ * These must be tested using the ApplicationTestBase, since the Symfony
+ * CommandTester does not fire Event Dispatchers.
+ *
+ * @package Acquia\Cli\Tests\Application
+ */
+class ExceptionApplicationTest extends TestBase {
+
+  public function setUp($output = NULL):void {
+    parent::setUp($output);
+    // If kernel is cached from a previous run, it doesn't get detected in code
+    // coverage reports.
+    $this->fs->remove('var/cache');
+  }
+
+  public function testInvalidApiCreds(): void {
+    $kernel = new Kernel('dev', 0);
+    $kernel->boot();
+    $container = $kernel->getContainer();
+    $container->set('datastore.cloud', $this->datastoreCloud);
+    $application = $container->get(Application::class);
+    $application->setAutoExit(FALSE);
+    $input = new ArrayInput(['link']);
+    $input->setInteractive(FALSE);
+    $output = new BufferedOutput();
+    $application->run($input, $output);
+    $buffer = $output->fetch();
+    // This is sensitive to the display width of the test environment, so that's fun.
+    $this->assertStringContainsString('Your Cloud Platform API credentials are invalid.', $buffer);  }
+
+}
--- tests/phpunit/src/Application/KernelTest.php
+++ tests/phpunit/src/Application/KernelTest.php
@@ -3,7 +3,6 @@
 namespace Acquia\Cli\Tests\Application;
 
 use Acquia\Cli\Kernel;
-use Acquia\Cli\Tests\TestBase;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
@@ -12,20 +11,18 @@ use Symfony\Component\Console\Output\BufferedOutput;
 /**
  * @package Acquia\Cli\Tests\Application
  */
-class KernelTest extends TestBase {
+class KernelTest extends TestCase {
 
-  public function setUp($output = NULL):void {
+  public function setUp():void {
     // If kernel is cached from a previous run, it doesn't get detected in code
     // coverage reports.
     shell_exec('rm -rf var/cache');
-    parent::setUp($output);
   }
 
   public function testRun(): void {
     $kernel = new Kernel('dev', 0);
     $kernel->boot();
     $container = $kernel->getContainer();
-    $container->set('datastore.cloud', $this->datastoreCloud);
     $application = $container->get(Application::class);
     $application->setAutoExit(FALSE);
     $input = new ArgvInput(['list']);
