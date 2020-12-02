e3e5c0c098c9cf1978395366b7f5f83c2b45ea7d
Matthew Grasmick <matt.grasmick@gmail.com>
2020-12-02 17:17:10 -0500

    Remove dir args from push and pull files commands.

--- src/Command/Pull/PullFilesCommand.php
+++ src/Command/Pull/PullFilesCommand.php
@@ -20,7 +20,6 @@ class PullFilesCommand extends PullCommandBase {
    */
   protected function configure() {
     $this->setDescription('Copy files from a Cloud Platform environment')
-      ->addArgument('dir', InputArgument::OPTIONAL, 'The directory containing the Drupal project to be refreshed')
       ->addArgument('environmentId', InputArgument::OPTIONAL, 'The UUID of the associated Cloud Platform source environment')
       ->setHidden(!AcquiaDrupalEnvironmentDetector::isAhIdeEnv() && !self::isLandoEnv());
   }
