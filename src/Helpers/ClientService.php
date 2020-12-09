f29cc39748d2bb0874925d7ee4041577623406f9
Dane Powell <git@danepowell.com>
2020-12-09 15:52:10 -0800

    DX-3045: Fix autowiring and don't reuse AcquiaCloudApi client

--- src/Helpers/ClientService.php
+++ src/Helpers/ClientService.php
@@ -4,32 +4,19 @@ namespace Acquia\Cli\Helpers;
 
 use AcquiaCloudApi\Connector\Client;
 use AcquiaCloudApi\Connector\Connector;
-use Webmozart\KeyValueStore\JsonFileStore;
 
 class ClientService {
 
-  private $acquiaCloudClient;
+  private $connector;
 
-  private $cloud_api_conf;
-
-  public function __construct(JsonFileStore $datastoreCloud) {
-    $this->cloud_api_conf = $datastoreCloud;
+  public function __construct(Connector $connector) {
+    $this->connector = $connector;
   }
 
   /**
    * @return \AcquiaCloudApi\Connector\Client
    */
   public function getClient(): Client {
-    if (isset($this->acquiaCloudClient)) {
-      return $this->acquiaCloudClient;
-    }
-
-    $config = [
-      'key' => $this->cloud_api_conf->get('key'),
-      'secret' => $this->cloud_api_conf->get('secret'),
-    ];
-    $this->acquiaCloudClient = Client::factory(new Connector($config));
-
-    return $this->acquiaCloudClient;
+    return Client::factory($this->connector);
   }
 }
