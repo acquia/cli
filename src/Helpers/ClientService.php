48895090295e3051a05087024c6d10a66d46bb6c
Dane Powell <git@danepowell.com>
2020-12-09 16:39:27 -0800

    Add comment

--- src/Helpers/ClientService.php
+++ src/Helpers/ClientService.php
@@ -5,6 +5,15 @@ namespace Acquia\Cli\Helpers;
 use AcquiaCloudApi\Connector\Client;
 use AcquiaCloudApi\Connector\Connector;
 
+/**
+ * Factory producing Acquia Cloud Api clients.
+ *
+ * This class is only necessary as a testing shim, so that we can prophesize
+ * client queries. Consumers could otherwise just call
+ * Client::factory($connector) directly.
+ *
+ * @package Acquia\Cli\Helpers
+ */
 class ClientService {
 
   private $connector;
