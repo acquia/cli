8bbfc356068759fac25598c5828632cc85751520
Matthew Grasmick <matt.grasmick@gmail.com>
2021-03-01 14:49:13 -0500

    Fixes #450: Add contextual information to exceptions.

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -6,6 +6,11 @@ use Acquia\Cli\Exception\AcquiaCliException;
 use AcquiaCloudApi\Exception\ApiErrorException;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Component\Console\Event\ConsoleErrorEvent;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ExceptionListener
 {
@@ -14,35 +19,41 @@ class ExceptionListener
     $exitCode = $event->getExitCode();
     $error = $event->getError();
     $errorMessage = $error->getMessage();
+    $io = new SymfonyStyle(new ArrayInput([]), new ConsoleOutput());
 
     // Make OAuth server errors more human-friendly.
     if ($error instanceof IdentityProviderException && $error->getMessage() === 'invalid_client') {
-      $newErrorMessage = 'Your Cloud Platform API credentials are invalid. Run acli auth:login to reset them.';
+      $new_error_message = 'Your Cloud Platform API credentials are invalid.';
+      $io->comment("Run <options=bold>acli auth:login</> to reset your API credentials.");
     }
 
     if ($error instanceof \Symfony\Component\Console\Exception\RuntimeException) {
       switch ($errorMessage) {
         case 'Not enough arguments (missing: "environmentId").':
-          $newErrorMessage = $errorMessage . PHP_EOL . 'Note: "environmentId" can also be an Acquia sitegroup and environment construct, e.g. myapp.dev.';
+          $io->comment('<options=bold>environmentId</> can also be an environment alias. E.g. <options=bold>myapp.dev</>.' . PHP_EOL
+            . 'Run <options=bold>acli remote:aliases:list</> to see a list of all available aliases.');
       }
     }
 
     if ($error instanceof ApiErrorException) {
       switch ($errorMessage) {
         case "There are no available Cloud IDEs for this application.\n":
-          $newErrorMessage = $errorMessage . "Delete an existing IDE (acli ide:delete) or contact your Account Manager or Acquia Sales to purchase additional IDEs.\n You may also submit a support ticket to ask for more information (https://insight.acquia.com/support/tickets/new?product=p:ride)";
+          $io->comment("Delete an existing IDE via <options=bold>acli ide:delete</> or contact your Account Manager or Acquia Sales to purchase additional IDEs." . PHP_EOL
+            . "You may also <href=https://insight.acquia.com/support/tickets/new?product=p:ride>submit a support ticket</> to ask for more information");
           break;
         // @todo Remove after CXAPI-8261 is closed.
         case "The Cloud IDE is being deleted.\n":
-          $newErrorMessage = "The Cloud IDE will be deleted momentarily. This process usually takes a few minutes.\n";
+          $new_error_message = "The Cloud IDE will be deleted momentarily. This process usually takes a few minutes." . PHP_EOL;
           break;
         default:
-          $newErrorMessage = 'Cloud Platform API returned an error: ' . $errorMessage;
+          $new_error_message = 'Cloud Platform API returned an error: ' . $errorMessage;
+          $io->comment("You can learn more about Cloud Platform API at <href=https://docs.acquia.com/cloud-platform/develop/api/>docs.acquia.com</>");
       }
     }
 
-    if (isset($newErrorMessage)) {
-      $event->setError(new AcquiaCliException($newErrorMessage, [], $exitCode));
+    $io->comment("You can find Acquia CLI documentation <href=https://docs.acquia.com/acquia-cli/>docs.acquia.com</>");
+    if (isset($new_error_message)) {
+      $event->setError(new AcquiaCliException($new_error_message, [], $exitCode));
     }
   }
 
