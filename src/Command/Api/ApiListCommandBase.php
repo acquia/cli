2dcff7cf19dbbd00c9571daf58b0d38badb2b682
Matthew Grasmick <matt.grasmick@gmail.com>
2021-03-01 11:12:50 -0500

    Add tests.

--- src/Command/Api/ApiListCommandBase.php
+++ src/Command/Api/ApiListCommandBase.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 /**
  *
  */
-abstract class ApiListCommandBase extends CommandBase {
+class ApiListCommandBase extends CommandBase {
 
   /**
    * @var string
