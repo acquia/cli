fc30b0b85003093201de1aa7b97e5560bc55f50c
Matthew Grasmick <matt.grasmick@gmail.com>
2021-03-01 21:04:55 -0500

    Replace import.

--- src/Command/Ide/IdeOpenCommand.php
+++ src/Command/Ide/IdeOpenCommand.php
@@ -4,7 +4,7 @@ namespace Acquia\Cli\Command\Ide;
 
 use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
 use AcquiaCloudApi\Endpoints\Ides;
-use http\Exception\RuntimeException;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -36,7 +36,7 @@ class IdeOpenCommand extends IdeCommandBase {
     $acquia_cloud_client = $this->cloudApiClientService->getClient();
     $cloud_application_uuid = $this->determineCloudApplication();
     if (!$cloud_application_uuid && !$input->isInteractive()) {
-      throw new \Symfony\Component\Console\Exception\RuntimeException('Not enough arguments (missing: "applicationUuid").');
+      throw new RuntimeException('Not enough arguments (missing: "applicationUuid").');
     }
     $ides_resource = new Ides($acquia_cloud_client);
     $ide = $this->promptIdeChoice("Please select the IDE you'd like to open:", $ides_resource, $cloud_application_uuid);
