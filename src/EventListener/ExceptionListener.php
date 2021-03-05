a42e191355952d87c167bccf1130d4634e0e69a4
Matthew Grasmick <matt.grasmick@gmail.com>
2021-03-05 13:34:12 -0500

    Suggest -vvv flag in help text. (#456)
    * Suggest -vvv flag in help text.
    
    * Check latest version.
    
    * Don't query GitHub from IDE.
    
    * Fix test.
    
    * Cleanup.
    
    * Upgrade consolidation/self-update.

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,11 +3,12 @@
 namespace Acquia\Cli\EventListener;
 
 use Acquia\Cli\Exception\AcquiaCliException;
+use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
 use AcquiaCloudApi\Exception\ApiErrorException;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Component\Console\Event\ConsoleErrorEvent;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class ExceptionListener {
 
@@ -64,15 +65,17 @@ class ExceptionListener {
       switch ($errorMessage) {
         case "There are no available Cloud IDEs for this application.\n":
           $this->helpMessages[] = "Delete an existing IDE via <bg={$this->messagesBgColor};options=bold>acli ide:delete</> or contact your Account Manager or Acquia Sales to purchase additional IDEs.";
-          $this->writeSupportTicketHelp();
           break;
         default:
           $new_error_message = 'Cloud Platform API returned an error: ' . $errorMessage;
+          $this->helpMessages[] = "You can learn more about Cloud Platform API at https://docs.acquia.com/cloud-platform/develop/api/";
       }
-      $this->helpMessages[] = "You can learn more about Cloud Platform API at https://docs.acquia.com/cloud-platform/develop/api/";
     }
 
     $this->helpMessages[] = "You can find Acquia CLI documentation at https://docs.acquia.com/acquia-cli/";
+    $this->writeUpdateHelp($event);
+    $this->writeSupportTicketHelp($event);
+
     if ($application = $event->getCommand()) {
       /** @var \Acquia\Cli\Application $application */
       $application = $event->getCommand()->getApplication();
@@ -101,11 +104,36 @@ class ExceptionListener {
   }
 
   /**
-   *
+   * @param $event
    */
-  protected function writeSupportTicketHelp(): void {
-    $this->helpMessages[] = "You may also ask for more information at" . PHP_EOL
-    . "https://insight.acquia.com/support/tickets/new?product=p:ride";
+  protected function writeSupportTicketHelp(ConsoleErrorEvent $event): void {
+    $message = "You can submit a support ticket at https://insight.acquia.com/support/tickets/new?product=p:cli";
+    if (!$event->getOutput()->isVeryVerbose()) {
+      $message .= PHP_EOL . "Please re-run the command with the <bg={$this->messagesBgColor};options=bold>-vvv</> flag and include the full command output in your support ticket.";
+    }
+    $this->helpMessages[] = $message;
+  }
+
+  /**
+   * @param \Symfony\Component\Console\Event\ConsoleErrorEvent $event
+   */
+  protected function writeUpdateHelp(ConsoleErrorEvent $event): void {
+    try {
+      /** @var \SelfUpdate\SelfUpdateCommand $self_update_command */
+      $self_update_command = $event->getCommand()
+        ->getApplication()
+        ->find('self-update');
+      [$latest, $downloadUrl] = $self_update_command->getLatest(FALSE);
+      // This will always be TRUE during dev because the package version is set to '@package_version@'.
+      $current_version = $event->getCommand()->getApplication()->getVersion();
+      if ($latest !== $current_version && !AcquiaDrupalEnvironmentDetector::isAhIdeEnv()) {
+        $message = "Acquia CLI {$latest} is available. Try updating via <bg={$this->messagesBgColor};options=bold>acli self-update</> and then run the command again.";
+        $this->helpMessages[] = $message;
+      }
+      // This command may not exist during some testing.
+    } catch (CommandNotFoundException $exception) {
+
+    }
   }
 
 }
