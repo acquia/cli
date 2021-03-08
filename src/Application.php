4f982401064c627bb2dd7942fe98cb224d68968e
Matthew Grasmick <matt.grasmick@gmail.com>
2021-03-08 11:57:27 -0500

    #Fixes: 464: Black text on dark blue background doesn't work for Mac users with the default white Terminal.

--- src/Application.php
+++ src/Application.php
@@ -40,7 +40,7 @@ class Application extends \Symfony\Component\Console\Application {
 
     if ($this->getHelpMessages()) {
       $io = new SymfonyStyle(new ArrayInput([]), $output);
-      $output_style = new OutputFormatterStyle(NULL, 'blue');
+      $output_style = new OutputFormatterStyle('white', 'blue');
       $output->getFormatter()->setStyle('help', $output_style);
       $io->block($this->getHelpMessages(), 'help', 'help', ' ', TRUE, FALSE);
     }
