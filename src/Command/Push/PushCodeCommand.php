6e3cd04d680c9fde5f63eace39a4239009f461a7
Dane Powell <git@danepowell.com>
2021-04-05 13:53:56 -0700

    Drush commands too

--- src/Command/Push/PushCodeCommand.php
+++ src/Command/Push/PushCodeCommand.php
@@ -205,7 +205,8 @@ class PushCodeCommand extends PullCommandBase {
       'docroot/modules/contrib',
       'docroot/themes/contrib',
       'docroot/profiles/contrib',
-      'docroot/libraries'
+      'docroot/libraries',
+      'drush/Commands'
     ];
   }
 
