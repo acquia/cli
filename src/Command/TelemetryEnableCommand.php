861f7e5adedd2d025f6e57226163a4c87e9cc8ed
Matthew Grasmick <matt.grasmick@gmail.com>
2021-04-07 12:49:10 -0400

    Fixes #511: Telemetry command should not be toggle.

---
+++ src/Command/TelemetryEnableCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Acquia\Cli\Command;
+
+use Acquia\Cli\Helpers\DataStoreContract;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class TelemetryEnableCommand.
+ */
+class TelemetryEnableCommand extends CommandBase {
+
+  protected static $defaultName = 'telemetry:enable';
+
+  /**
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   *
+   * @return bool
+   */
+  protected function commandRequiresAuthentication(InputInterface $input): bool {
+    return FALSE;
+  }
+
+  /**
+   * {inheritdoc}.
+   */
+  protected function configure() {
+    $this->setDescription('Enable anonymous sharing of usage and performance data');
+  }
+
+  /**
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *
+   * @return int 0 if everything went fine, or an exit code
+   * @throws \Exception
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $datastore = $this->datastoreCloud;
+    $datastore->set(DataStoreContract::SEND_TELEMETRY, TRUE);
+    $this->io->success('<info>Telemetry has been enabled.</info>');
+
+    return 0;
+  }
+
+}
