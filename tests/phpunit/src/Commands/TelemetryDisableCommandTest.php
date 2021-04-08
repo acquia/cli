f23f0d13cb1eabea6e303a5944822d2160547524
Matthew Grasmick <matthew.grasmick@acquia.com>
2021-04-08 12:47:38 -0400

    Fixes #511: Telemetry command should not be toggle. (#512)

---
+++ tests/phpunit/src/Commands/TelemetryDisableCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Acquia\Cli\Tests\Commands;
+
+use Acquia\Cli\Command\TelemetryDisableCommand;
+use Acquia\Cli\Tests\CommandTestBase;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class TelemetryDisableCommandTest.
+ *
+ * @property \Acquia\Cli\Command\TelemetryDisableCommand $command
+ * @package Acquia\Cli\Tests\Commands
+ */
+class TelemetryDisableCommandTest extends CommandTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createCommand(): Command {
+    return $this->injectCommand(TelemetryDisableCommand::class);
+  }
+
+  /**
+   * Tests the 'telemetry:disable' command.
+   */
+  public function testTelemetryDisableCommand(): void {
+    $this->executeCommand();
+    $output = $this->getDisplay();
+    $this->assertStringContainsString('Telemetry has been disabled.', $output);
+
+    $settings = json_decode(file_get_contents($this->cloudConfigFilepath), TRUE);
+    $this->assertFalse($settings['send_telemetry']);
+  }
+
+}
---
+++ tests/phpunit/src/Commands/TelemetryEnableCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Acquia\Cli\Tests\Commands;
+
+use Acquia\Cli\Command\TelemetryEnableCommand;
+use Acquia\Cli\Tests\CommandTestBase;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class TelemetryEnableCommandTest.
+ *
+ * @property \Acquia\Cli\Command\TelemetryEnableCommand $command
+ * @package Acquia\Cli\Tests\Commands
+ */
+class TelemetryEnableCommandTest extends CommandTestBase {
+
+  /**b
+   * {@inheritdoc}
+   */
+  protected function createCommand(): Command {
+    return $this->injectCommand(TelemetryEnableCommand::class);
+  }
+
+  /**
+   * Tests the 'telemetry:enable' command.
+   */
+  public function testTelemetryEnableCommand(): void {
+    $this->executeCommand();
+    $output = $this->getDisplay();
+    $this->assertStringContainsString('Telemetry has been enabled.', $output);
+
+    $settings = json_decode(file_get_contents($this->cloudConfigFilepath), TRUE);
+    $this->assertTrue($settings['send_telemetry']);
+  }
+
+}
