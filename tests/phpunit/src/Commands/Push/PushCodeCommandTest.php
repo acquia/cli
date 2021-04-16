fd802adf25c2e3812afb0fcb544be67d9d5c1e70
Dane Powell <git@danepowell.com>
2021-04-16 09:57:46 -0700

    Restore tests

--- tests/phpunit/src/Commands/Push/PushCodeCommandTest.php
+++ tests/phpunit/src/Commands/Push/PushCodeCommandTest.php
@@ -2,29 +2,14 @@
 
 namespace Acquia\Cli\Tests\Commands\Push;
 
-use Acquia\Cli\Command\Ide\IdePhpVersionCommand;
-use Acquia\Cli\Command\Pull\PullCodeCommand;
-use Acquia\Cli\Command\Pull\PullCommand;
-use Acquia\Cli\Command\Pull\PullDatabaseCommand;
-use Acquia\Cli\Command\Pull\PullFilesCommand;
-use Acquia\Cli\Command\Push\PushArtifactCommand;
-use Acquia\Cli\Command\Push\PushFilesCommand;
-use Acquia\Cli\Exception\AcquiaCliException;
-use Acquia\Cli\Helpers\SshHelper;
-use Acquia\Cli\Tests\Commands\Ide\IdeRequiredTestBase;
+use Acquia\Cli\Command\Push\PushCodeCommand;
 use Acquia\Cli\Tests\CommandTestBase;
-use AcquiaCloudApi\Response\EnvironmentResponse;
-use Prophecy\Argument;
-use Prophecy\Prophecy\ObjectProphecy;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\Process\Process;
-use Webmozart\PathUtil\Path;
 
 /**
  * Class PushCodeCommandTest.
  *
- * @property \Acquia\Cli\Command\Push\PushArtifactCommand $command
+ * @property \Acquia\Cli\Command\Push\PushCodeCommand $command
  * @package Acquia\Cli\Tests\Commands\Push
  */
 class PushCodeCommandTest extends CommandTestBase {
@@ -33,7 +18,7 @@ class PushCodeCommandTest extends CommandTestBase {
    * {@inheritdoc}
    */
   protected function createCommand(): Command {
-    return $this->injectCommand(PushArtifactCommand::class);
+    return $this->injectCommand(PushCodeCommand::class);
   }
 
   public function testPushCode(): void {
