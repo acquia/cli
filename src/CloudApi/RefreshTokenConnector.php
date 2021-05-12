2ebd6a6e4ac2a57b355ffbb8a2919f51b0735fc4
Matthew Grasmick <matt.grasmick@gmail.com>
2021-05-12 16:20:00 -0400

    Add more coverage.

--- src/CloudApi/RefreshTokenConnector.php
+++ src/CloudApi/RefreshTokenConnector.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client as GuzzleClient;
 use League\OAuth2\Client\Provider\GenericProvider;
 use League\OAuth2\Client\Token\AccessTokenInterface;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\CacheInterface;
 use Webmozart\PathUtil\Path;
 
 /**
@@ -40,6 +41,11 @@ class RefreshTokenConnector implements ConnectorInterface {
   protected $refreshToken;
 
   /**
+   * @var \Symfony\Component\Cache\Adapter\FilesystemAdapter
+   */
+  private $cache;
+
+  /**
    * @inheritdoc
    */
   public function __construct(array $config, string $base_uri = NULL) {
@@ -47,6 +53,8 @@ class RefreshTokenConnector implements ConnectorInterface {
     if ($base_uri) {
       $this->baseUri = $base_uri;
     }
+    $cache_directory = sprintf('%s%s%s', Path::getHomeDirectory(), \DIRECTORY_SEPARATOR, '.acquia-php-sdk-v2');
+    $this->cache = new FilesystemAdapter('cache', 300, $cache_directory);
 
     $this->refreshToken = $config['refreshToken'];
     $this->provider = new GenericProvider([
@@ -70,9 +78,7 @@ class RefreshTokenConnector implements ConnectorInterface {
    */
   public function createRequest($verb, $path) {
     if (!isset($this->accessToken) || $this->accessToken->hasExpired()) {
-      $directory = sprintf('%s%s%s', Path::getHomeDirectory(), \DIRECTORY_SEPARATOR, '.acquia-php-sdk-v2');
-      $cache = new FilesystemAdapter('cache', 300, $directory);
-      $accessToken = $cache->get('cloudapi-token', function () {
+      $accessToken = $this->cache->get('cloudapi-token', function () {
         return $this->provider->getAccessToken('refresh_token', [
           'refresh_token' => $this->refreshToken,
         ]);
@@ -94,6 +100,27 @@ class RefreshTokenConnector implements ConnectorInterface {
   public function sendRequest($verb, $path, $options) {
     $request = $this->createRequest($verb, $path);
     return $this->client->send($request, $options);
+  }
+
+  /**
+   * @param \League\OAuth2\Client\Provider\GenericProvider $provider
+   */
+  public function setProvider(GenericProvider $provider): void {
+    $this->provider = $provider;
+  }
+
+  /**
+   * @param \GuzzleHttp\Client $client
+   */
+  public function setClient(GuzzleClient $client): void {
+    $this->client = $client;
+  }
+
+  /**
+   * @param \Symfony\Contracts\Cache\CacheInterface $cache
+   */
+  public function setCache(CacheInterface $cache): void {
+    $this->cache = $cache;
   }
 
 }
--- tests/phpunit/src/CloudApi/RefreshTokenConnectorTest.php
+++ tests/phpunit/src/CloudApi/RefreshTokenConnectorTest.php
@@ -5,6 +5,14 @@ namespace Acquia\Cli\Tests\CloudApi;
 use Acquia\Cli\CloudApi\ConnectorFactory;
 use Acquia\Cli\CloudApi\RefreshTokenConnector;
 use Acquia\Cli\Tests\TestBase;
+use AcquiaCloudApi\Connector\ConnectorInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use League\OAuth2\Client\Provider\GenericProvider;
+use League\OAuth2\Client\Token\AccessTokenInterface;
+use Prophecy\Argument;
+use Psr\Http\Message\RequestInterface;
+use Symfony\Component\Cache\Adapter\NullAdapter;
 
 /**
  * Class RefreshTokenTest.
@@ -38,6 +46,29 @@ class RefreshTokenConnectorTest extends TestBase {
       $this->cloudCredentials->getBaseUri());
     $connector = $connector_factory->createConnector();
     self::assertInstanceOf(RefreshTokenConnector::class, $connector);
+
+    $verb = 'get';
+    $path = 'api';
+    $options = [];
+
+    $mock_client = $this->prophet->prophesize(Client::class);
+    $mock_client->send(Argument::type(RequestInterface::class), Argument::type('array'))->shouldBeCalled();
+    $connector->setClient($mock_client->reveal());
+
+    $mock_provider = $this->prophet->prophesize(GenericProvider::class);
+    $mock_provider->getAccessToken('refresh_token', ['refresh_token' => $this->refreshToken])
+      ->willReturn($this->prophet->prophesize(AccessTokenInterface::class)->reveal())
+      ->shouldBeCalled();
+    $mock_provider->getAuthenticatedRequest($verb, Argument::type('string'), Argument::type(AccessTokenInterface::class))
+      ->willReturn($this->prophet->prophesize(RequestInterface::class)->reveal())
+      ->shouldBeCalled();
+    $connector->setProvider($mock_provider->reveal());
+
+    $connector->setCache(new NullAdapter());
+
+    $connector->sendRequest($verb, $path, $options);
+
+    $this->prophet->checkPredictions();
   }
 
 }
