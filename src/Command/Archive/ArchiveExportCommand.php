cdee209563289157402698f7b93b1f4a37cab078
Matthew Grasmick <matthew.grasmick@acquia.com>
2021-09-09 20:26:57 -0400

    Update src/Command/Archive/ArchiveExportCommand.php

--- src/Command/Archive/ArchiveExportCommand.php
+++ src/Command/Archive/ArchiveExportCommand.php
@@ -187,6 +187,7 @@ class ArchiveExportCommand extends PullCommandBase {
   protected function compressArchiveDirectory($archive_dir, $destination_dir, $output_callback = NULL): string {
     $destination_filename = basename($archive_dir) . '.tar.gz';
     $destination_filepath = Path::join([$destination_dir, $destination_filename]);
+    $this->localMachineHelper->checkRequiredBinariesExist(['tar']);
     $process = $this->localMachineHelper->execute(['tar', '-zcvf', $destination_filepath, '--directory', $archive_dir, '.'], $output_callback, NULL, ($this->output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL));
     if (!$process->isSuccessful()) {
       throw new AcquiaCliException('Unable to create tarball: {message}', ['message' => $process->getErrorOutput()]);
