32816b8ca83ae683bd96c04f9594f8e2a859ef56
Matthew Grasmick <matt.grasmick@gmail.com>
2021-10-12 10:43:16 -0400

    Don't show deprecated commands.

--- src/Command/Api/ApiCommandHelper.php
+++ src/Command/Api/ApiCommandHelper.php
@@ -497,6 +497,10 @@ class ApiCommandHelper {
       if (array_key_exists('x-internal', $endpoint) && $endpoint['x-internal']) {
         continue;
       }
+      // Skip deprecated endpoints.
+      if (array_key_exists('deprecated', $endpoint) && $endpoint['deprecated']) {
+        continue;
+      }
 
       foreach ($endpoint as $method => $schema) {
         if (in_array($schema['x-cli-name'], $this->getSkippedApiCommands(), TRUE)) {
