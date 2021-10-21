5666301b3bd73ac7537b5f08330ab7b078e6d0ec
Aurelien Navarre <hello@anavarre.net>
2021-10-21 10:30:00 +0200

    Be less aggressive with execute method removal

--- src/Command/Pull/PullCommandBase.php
+++ src/Command/Pull/PullCommandBase.php
@@ -90,6 +90,18 @@ abstract class PullCommandBase extends CommandBase {
    * @param \Symfony\Component\Console\Input\InputInterface $input
    * @param \Symfony\Component\Console\Output\OutputInterface $output
    *
+   * @return int 0 if everything went fine, or an exit code
+   * @throws \Exception
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    return 0;
+  }
+
+  /**
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *
    * @throws \Acquia\Cli\Exception\AcquiaCliException
    * @throws \Exception
    */
