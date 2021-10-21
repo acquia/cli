109771961cc420937e8e481efe7821f005f31200
Dane Powell <git@danepowell.com>
2021-10-21 12:22:27 -0700

    Check disk space before pulling files (#695)

--- tests/phpunit/src/Commands/Pull/PullFilesCommandTest.php
+++ tests/phpunit/src/Commands/Pull/PullFilesCommandTest.php
@@ -29,6 +29,7 @@ class PullFilesCommandTest extends PullCommandTestBase {
     $applications_response = $this->mockApplicationsRequest();
     $this->mockApplicationRequest();
     $environments_response = $this->mockAcsfEnvironmentsRequest($applications_response);
+    $selected_environment = $environments_response->_embedded->items[0];
     $ssh_helper = $this->mockSshHelper();
     $this->mockGetAcsfSites($ssh_helper);
     $local_machine_helper = $this->mockLocalMachineHelper();
@@ -36,8 +37,7 @@ class PullFilesCommandTest extends PullCommandTestBase {
       ->getFilesystem()
       ->willReturn($this->fs)
       ->shouldBeCalled();
-    $process = $this->mockProcess();
-    $this->mockExecuteAcsfRsync($local_machine_helper, $environments_response, $process);
+    $this->mockExecuteRsync($local_machine_helper, $ssh_helper, $selected_environment, '/mnt/files/profserv2.dev/sites/g/files/jxr5000596dev/files', $this->projectFixtureDir . '/docroot/sites/jxr5000596dev/');
 
     $this->command->localMachineHelper = $local_machine_helper->reveal();
     $this->command->sshHelper = $ssh_helper->reveal();
@@ -77,8 +77,8 @@ class PullFilesCommandTest extends PullCommandTestBase {
       ->getFilesystem()
       ->willReturn($this->fs)
       ->shouldBeCalled();
-    $process = $this->mockProcess();
-    $this->mockExecuteCloudRsync($local_machine_helper, $process, $selected_environment);
+    $sitegroup = CommandBase::getSiteGroupFromSshUrl($selected_environment->ssh_url);
+    $this->mockExecuteRsync($local_machine_helper, $ssh_helper, $selected_environment, '/mnt/files/' . $sitegroup . '.' . $selected_environment->name . '/sites/bar/files', $this->projectFixtureDir . '/docroot/sites/bar/');
 
     $this->command->localMachineHelper = $local_machine_helper->reveal();
     $this->command->sshHelper = $ssh_helper->reveal();
@@ -121,45 +121,38 @@ class PullFilesCommandTest extends PullCommandTestBase {
 
   /**
    * @param \Prophecy\Prophecy\ObjectProphecy $local_machine_helper
-   * @param object $environments_response
-   * @param \Prophecy\Prophecy\ObjectProphecy $process
-   */
-  protected function mockExecuteAcsfRsync(
-    ObjectProphecy $local_machine_helper,
-    $environments_response,
-    ObjectProphecy $process
-  ): void {
-    $local_machine_helper->checkRequiredBinariesExist(['rsync'])->shouldBeCalled();
-    $command = [
-      'rsync',
-      '-rltDvPhe',
-      'ssh -o StrictHostKeyChecking=no',
-      'profserv2.01dev@profserv201dev.ssh.enterprise-g1.acquia-sites.com:/mnt/files/profserv2.dev/sites/g/files/jxr5000596dev/files',
-      $this->projectFixtureDir . '/docroot/sites/jxr5000596dev/',
-    ];
-    $local_machine_helper->execute($command, Argument::type('callable'), NULL, FALSE, 60 * 60)
-      ->willReturn($process->reveal())
-      ->shouldBeCalled();
-  }
-
-  /**
-   * @param \Prophecy\Prophecy\ObjectProphecy $local_machine_helper
    * @param \Prophecy\Prophecy\ObjectProphecy $process
    * @param $environment
+   * @param $source_dir
+   * @param $destination_dir
    */
-  protected function mockExecuteCloudRsync(
+  protected function mockExecuteRsync(
     ObjectProphecy $local_machine_helper,
-    ObjectProphecy $process,
-    $environment
+    ObjectProphecy $ssh_helper,
+    $environment,
+    string $source_dir,
+    string $destination_dir
   ): void {
+    $process = $this->mockProcess();
+    $process->getOutput()->willReturn("123\tfiles")->shouldBeCalled();
+    $local_machine_helper->execute(['du', '-s', $destination_dir . 'files'], NULL, NULL, FALSE)
+      ->willReturn($process->reveal())->shouldBeCalled();
+    $process = $this->mockProcess();
+    $process->getOutput()->willReturn("123\tsomething")->shouldBeCalled();
+    $ssh_helper->executeCommand(Argument::any(), ['du', '-s', $source_dir], FALSE)
+      ->willReturn($process->reveal())->shouldBeCalled();
+    $process = $this->mockProcess();
+    $process->getOutput()->willReturn("\tAvail\n12345")->shouldBeCalled();
+    $local_machine_helper->execute(['df', '--output=avail', '-k', $destination_dir . 'files'], NULL, NULL, FALSE)
+      ->willReturn($process->reveal())->shouldBeCalled();
+
     $local_machine_helper->checkRequiredBinariesExist(['rsync'])->shouldBeCalled();
-    $sitegroup = CommandBase::getSiteGroupFromSshUrl($environment->ssh_url);
     $command = [
       'rsync',
       '-rltDvPhe',
       'ssh -o StrictHostKeyChecking=no',
-      $environment->ssh_url . ':/mnt/files/' . $sitegroup . '.' . $environment->name . '/sites/bar/files',
-      $this->projectFixtureDir . '/docroot/sites/bar/',
+      $environment->ssh_url . ':' . $source_dir,
+      $destination_dir
     ];
     $local_machine_helper->execute($command, Argument::type('callable'), NULL, FALSE, 60 * 60)
       ->willReturn($process->reveal())
