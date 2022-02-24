cd1cbca0d704583adf59ce5cf0b28fa0c4cbdf90
Matthew Grasmick <matt.grasmick@gmail.com>
2022-02-24 12:08:37 -0500

    CLI-662: Add open:app command. (#823)

---
+++ src/Command/App/AppOpenCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acquia\Cli\Command\App;
+
+use Acquia\Cli\Command\CommandBase;
+use Acquia\Cli\Helpers\LocalMachineHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class AppOpenCommand.
+ */
+class AppOpenCommand extends CommandBase {
+
+  protected static $defaultName = 'app:open';
+
+  /**
+   * {inheritdoc}.
+   */
+  protected function configure() {
+    $this->setDescription('Opens your browser to view a given Cloud application')
+      ->acceptApplicationUuid()
+      ->setHidden(!LocalMachineHelper::isBrowserAvailable())
+      ->setAliases(['open', 'o']);
+  }
+
+  /**
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *
+   * @return int 0 if everything went fine, or an exit code
+   * @throws \Acquia\Cli\Exception\AcquiaCliException
+   * @throws \Exception
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $application_uuid = $this->determineCloudApplication();
+    $this->localMachineHelper->startBrowser('https://cloud.acquia.com/a/applications/' . $application_uuid);
+
+    return 0;
+  }
+
+}
