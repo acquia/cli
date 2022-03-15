72fb4d589092ef3aaf087cb99135b31e85a0b040
joshirohit100 <joshi.rohit100@gmail.com>
2022-03-15 21:43:06 +0530

    Adding other products as well for doc.

--- src/Command/DocsCommand.php
+++ src/Command/DocsCommand.php
@@ -4,6 +4,7 @@ namespace Acquia\Cli\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 /**
  * Class DocsCommand.
@@ -16,7 +17,7 @@ class DocsCommand extends CommandBase {
    * {inheritdoc}.
    */
   protected function configure() {
-    $this->setDescription('Open Acquia CLI documentation in a web browser');
+    $this->setDescription('Open Acquia products documentation in a web browser');
   }
 
   /**
@@ -26,7 +27,26 @@ class DocsCommand extends CommandBase {
    * @return int
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $this->localMachineHelper->startBrowser('https://docs.acquia.com/acquia-cli/');
+    $acquia_products = [
+      'Acquia CLI' => 'acquia-cli',
+      'Acquia CMS' => 'acquia-cms',
+      'Code Studio' => 'code-studio',
+      'Campaign Studio' => 'campaign-studio',
+      'Content Hub' => 'contenthub',
+      'Acquia Migrate Accelerate' => 'acquia-migrate-accelerate',
+      'Site Factory' => 'site-factory',
+      'Site Studio' => 'site-studio',
+      'Edge' => 'edge',
+      'Search' => 'acquia-search',
+      'Shield' => 'shield',
+      'Customer Data Plateform' => 'customer-data-platform',
+      'Cloud IDE' => 'ide',
+    ];
+
+    $labels = array_keys($acquia_products);
+    $question = new ChoiceQuestion('Please select the Acquia Product', $labels, $labels[0]);
+    $choice_id = $this->io->askQuestion($question);
+    $this->localMachineHelper->startBrowser('https://docs.acquia.com/' . $acquia_products[$choice_id] . '/');
 
     return 0;
   }
