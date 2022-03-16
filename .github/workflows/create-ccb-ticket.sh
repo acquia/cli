51a5e7bc77e8688b9474845c6b63e2ce0a139ebb
Matthew Grasmick <matt.grasmick@gmail.com>
2022-03-16 15:45:16 -0400

    CLI-684: Automate CCB ticket creation for pre-releases. (#840)
    * Create release.yml
    
    * Update release.yml
    
    * Add secrets
    
    * Create ccb-ticket.twig
    
    * Add a twig templaet.
    
    * Fix twig rendering.
    
    * Do it in PHP.
    
    * Cleanup
    
    * Rename
    
    * Update vars.
    
    * Use personal access token.
    
    * Update.
    
    * Revert "Use personal access token."
    
    This reverts commit 6d3fa6d86e06f9a6cc45cdb3cc99dfc2bd72ea63.
    
    * Use native action.
    
    * Composer updae.
    
    * Remove extra files.
    
    * Update github issue title.
    
    * Update .github/workflows/create-issue.yml
    
    * Update .github/workflows/release.yml
    
    * Update .github/workflows/release.yml
    
    * Test.
    
    * Test
    
    * Disable helper action.
    
    * Simplify.
    
    * Uncomment.
    
    * CLI
    
    * Update .github/workflows/release.yml
    
    Co-authored-by: Dane Powell <git@danepowell.com>
    
    * Update .github/workflows/release.yml
    
    Co-authored-by: Dane Powell <git@danepowell.com>
    
    Co-authored-by: Dane Powell <git@danepowell.com>

---
+++ .github/workflows/create-ccb-ticket.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__);
+$twig = new Environment($loader);
+$body = $twig->render('ccb-ticket.twig', [
+   // @see https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+  'GITHUB_RELEASE_BODY' => $_ENV['GITHUB_RELEASE_BODY'],
+  'GITHUB_RELEASE_NAME' => $_ENV['GITHUB_RELEASE_NAME'],
+  'JIRA_BASE_URL' => $_ENV['JIRA_BASE_URL'],
+  'GITHUB_ACTIONS_RUN_URL' => $_ENV['GITHUB_ACTIONS_RUN_URL'],
+]);
+echo $body;
