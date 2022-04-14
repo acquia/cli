85cad15d39841da8b42b1bb5104e12f0dfa8957a
Dane Powell <git@danepowell.com>
2022-04-14 08:37:49 -0700

    Revert "remove broken tests"
    This reverts commit ae08062aca34ac10ed436b26e106b721ba70a307.

---
+++ tests/phpunit/src/Commands/DocsCommandTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Acquia\Cli\Tests\Commands;
+
+use Acquia\Cli\Command\DocsCommand;
+use Acquia\Cli\Tests\CommandTestBase;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class DocsCommandTest.
+ *
+ * @property \Acquia\Cli\Command\DocsCommandTest $command
+ * @package Acquia\Cli\Tests\Commands
+ */
+class DocsCommandTest extends CommandTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createCommand(): Command {
+    return $this->injectCommand(DocsCommand::class);
+  }
+
+  /**
+   * Tests the 'docs' command for Acquia Products.
+   *
+   * @dataProvider providerTestDocsCommand
+   *
+   * @throws \Exception
+   */
+  public function testDocsCommand($input, $expectedOutput): void {
+    $this->executeCommand([], [$input]);
+    $output = $this->getDisplay();
+    $this->assertStringContainsString('Please select the Acquia Product [Acquia CLI]:', $output);
+    $this->assertStringContainsString($expectedOutput, $output);
+  }
+
+  public function providerTestDocsCommand(): array {
+    return [
+      [
+        0,
+        '[0 ] Acquia CLI',
+      ],
+      [
+        1,
+        '[1 ] Acquia CMS',
+      ],
+      [
+        2,
+        '[2 ] Code Studio',
+      ],
+      [
+        3,
+        '[3 ] Campaign Studio',
+      ],
+      [
+        4,
+        '[4 ] Content Hub',
+      ],
+      [
+        5,
+        '[5 ] Acquia Migrate Accelerate',
+      ],
+      [
+        6,
+        '[6 ] Site Factory',
+      ],
+      [
+        7,
+        '[7 ] Site Studio',
+      ],
+      [
+        8,
+        '[8 ] Edge',
+      ],
+      [
+        9,
+        '[9 ] Search',
+      ],
+      [
+        10,
+        '[10] Shield',
+      ],
+      [
+        11,
+        '[11] Customer Data Plateform',
+      ],
+      [
+        12,
+        '[12] Cloud IDE',
+      ],
+      [
+        13,
+        '[13] BLT',
+      ],
+      [
+        14,
+        '[14] Cloud Platform',
+      ],
+      [
+        15,
+        '[15] Acquia DAM Classic',
+      ],
+      [
+        16,
+        '[16] Personalization',
+      ],
+      [
+        17,
+        '[17] Campaign Factory',
+      ],
+    ];
+  }
+
+}
