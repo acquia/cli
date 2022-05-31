f75e94a2cfcf9474248dcbb85bed75163851e34e
Matthew Grasmick <matt.grasmick@gmail.com>
2022-05-31 10:50:26 -0400

    Fixes CLI-775: Cast array parameters to array. (#1020)
    * Fixes CLI-775: Cast array parameters to array.
    
    * Test.

--- src/Command/Api/ApiBaseCommand.php
+++ src/Command/Api/ApiBaseCommand.php
@@ -256,19 +256,12 @@ class ApiBaseCommand extends CommandBase {
       return $value;
     }
 
-    switch ($type) {
-      case 'int':
-      case 'integer':
-        $value = (int) $value;
-        break;
-
-      case 'bool':
-      case 'boolean':
-        $value = (bool) $value;
-        break;
-    }
-
-    return $value;
+    return match ($type) {
+      'int', 'integer' => (int) $value,
+      'bool', 'boolean' => (bool) $value,
+      'array' => (array) $value,
+      'string' => (string) $value,
+    };
   }
 
   /**
@@ -304,6 +297,9 @@ class ApiBaseCommand extends CommandBase {
         if (in_array($type, ['int', 'integer'])) {
           // Need to evaluate whether a string contains only digits.
           $constraints[] = new Type('digit');
+        }
+        elseif ($type === 'array') {
+          $constraints[] = new Type('string');
         }
         else {
           $constraints[] = new Type($type);
