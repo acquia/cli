2af4e1e672d56fd56f22e94baba6219ebfa308ac
Rahul Gupta <rahul.gupta@acquia.com>
2022-06-09 19:03:44 +0530

    RADX-640 | Code improvment

--- src/Command/DrupalUpdate/DrupalUpdateCommand.php
+++ src/Command/DrupalUpdate/DrupalUpdateCommand.php
@@ -3,54 +3,91 @@
 
 namespace Acquia\Cli\Command\DrupalUpdate;
 
+
 use Acquia\Cli\Command\CommandBase;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+
 
 class DrupalUpdateCommand extends  CommandBase
 {
-  protected static $defaultName = 'd7-update';
+  protected static $defaultName = 'app:update-d7-packages';
+  private $drupalRootPath;
+
+    /**
+     * @return mixed
+     */
+    public function getDrupalRootPath()
+    {
+        return $this->drupalRootPath;
+    }
+
+    /**
+     * @param mixed $drupalRootPath
+     */
+    public function setDrupalRootPath($drupalRootPath): void
+    {
+        $this->drupalRootPath = $drupalRootPath;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDrupalCoreVersion()
+    {
+        return $this->drupalCoreVersion;
+    }
+
+    /**
+     * @param mixed $drupalCoreVersion
+     */
+    public function setDrupalCoreVersion($drupalCoreVersion): void
+    {
+        $this->drupalCoreVersion = $drupalCoreVersion;
+    }
+  private $drupalCoreVersion;
 
   /**
    * {inheritdoc}.
    */
   protected function configure() {
-    $this->setDescription('Drupal 7 update codebase.')
-            ->addOption('docroot-path', NULL, InputOption::VALUE_REQUIRED, 'Drupal docroot path.')
+    $this->setDescription('Updates modules, themes, and distributions for a Drupal 7 application.')
             ->setHidden(TRUE);
   }
 
-  /**
-   * @param \Symfony\Component\Console\Input\InputInterface $input
-   * @param \Symfony\Component\Console\Output\OutputInterface $output
-   *
-   * @return int
-   */
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int
+     * @throws \Acquia\Cli\Exception\AcquiaCliException
+     */
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $this->io->success('Start D7 update checking.');
-    $path = $input->getOption('docroot-path');
-    if(!isset($path)){
-      define('DRUPAL_ROOT', getcwd());
-    }else{
-      define('DRUPAL_ROOT', $path);
+    $this->validateCwdIsValidDrupalProject();
+    $this->io->note('Start checking of available updates.');
+    $this->setDrupalRootPath(getcwd());
+    $drupal_root_path = $this->getDrupalRootPath();
+    if(file_exists($drupal_root_path . '/docroot/includes/bootstrap.inc')){
+      $boostrap_file_contents = file_get_contents($drupal_root_path . '/docroot/includes/bootstrap.inc');
+      preg_match("/define\(\s*'([^']*)'\s*,\s*'([^']*)'\s*\)/i", $boostrap_file_contents, $constraint_matches);
+      if((count($constraint_matches)>2) && ($constraint_matches[1]=='VERSION')){
+          $this->setDrupalCoreVersion($constraint_matches[2]);
+      }
     }
 
-    if(file_exists(DRUPAL_ROOT . '/docroot/includes/bootstrap.inc')){
-      include_once DRUPAL_ROOT . '/docroot/includes/bootstrap.inc';
-    }
-    $updatescript = new UpdateScript();
-    // get all .info files
-    $updatescript->getInfoFilesList();
+    $package_update_script = new PackageUpdateScript($this->drupalRootPath, $this->io,$this->drupalCoreVersion);
+    $this->io->note('Reading all packages .info files.');
+    $package_update_script->getInfoFilesList();
 
-    // get all modules, themes, profiles details.
-    $updatescript->getPackageDetailInfo();
+    $this->io->note('Preparing all packages detail list(package name, package type,current version etc.).');
+    $package_update_script->getPackageDetailInfo();
 
-    // get available security updates of core and contrib.
-    $latest_updates = $updatescript->securityUpdateVersion();
 
-    // update and show updated the details & remove the downloaded tar files.
-    $updatescript->updateAvailableUpdates($latest_updates);
+    $this->io->note('Checking available updates of packages.');
+    $latest_updates = $package_update_script->securityUpdateVersion();
+
+    $this->io->note('Updated packages details');
+    $package_update_script->updateAvailableUpdates($latest_updates);
     return 0;
   }
 
