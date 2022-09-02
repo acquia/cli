8f222ad7c2f8db23c864c38cafc1e25382c3a524
Dane Powell <git@danepowell.com>
2022-09-02 13:47:33 -0700

    CLI-842: Restore single-db imports for IDE (#1159)

--- src/Command/Pull/PullCommandBase.php
+++ src/Command/Pull/PullCommandBase.php
@@ -1063,6 +1063,11 @@ abstract class PullCommandBase extends CommandBase {
       $this->io->note("Acquia CLI assumes that the database name for the {$database->name} database is also {$database->name}");
       $this->doImportRemoteDatabase($this->getDefaultLocalDbHost(), 'root', $database->name, '', $local_filepath, $output_callback);
     }
+    elseif (AcquiaDrupalEnvironmentDetector::isAhIdeEnv() && !getenv('IDE_ENABLE_MULTISITE')) {
+      // Cloud IDE only has 2 available databases for importing, so we only allow importing into the default database.
+      $this->io->note("Cloud IDE only supports importing into the default Drupal database. Acquia CLI will import the NON-DEFAULT database {$database->name} into the DEFAULT database {$this->getDefaultLocalDbName()}");
+      $this->doImportRemoteDatabase($this->getDefaultLocalDbHost(), $this->getDefaultLocalDbUser(), $this->getDefaultLocalDbName(), $this->getDefaultLocalDbPassword(), $local_filepath, $output_callback);
+    }
     else {
       $this->io->note("Acquia CLI assumes that the local database name for the {$database->name} database is also {$database->name}");
       $this->doImportRemoteDatabase($this->getDefaultLocalDbHost(), $this->getDefaultLocalDbUser(), $database->name, $this->getDefaultLocalDbPassword(), $local_filepath, $output_callback);
