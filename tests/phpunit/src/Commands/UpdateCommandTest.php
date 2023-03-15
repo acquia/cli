cd1c9e5357120ee6d2593966ef5706419f1fb79c
Dane Powell <git@danepowell.com>
2023-03-14 21:12:24 -0700

    another test

--- tests/phpunit/src/Commands/UpdateCommandTest.php
+++ tests/phpunit/src/Commands/UpdateCommandTest.php
@@ -4,6 +4,8 @@ namespace Acquia\Cli\Tests\Commands;
 
 use Acquia\Cli\Command\HelloWorldCommand;
 use Acquia\Cli\Tests\CommandTestBase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\Console\Command\Command;
 
 /**
@@ -29,9 +31,20 @@ class UpdateCommandTest extends CommandTestBase {
     self::assertStringContainsString('Acquia CLI 2.8.5 is available', $this->getDisplay());
   }
 
-  public function testSilentFailure(): void {
+  public function testBadResponseFailsSilently(): void {
     $this->setUpdateClient(403);
     $this->application->setVersion('2.8.4');
+    $this->executeCommand([], []);
+    self::assertEquals(0, $this->getStatusCode());
+    self::assertStringNotContainsString('Acquia CLI 2.8.5 is available', $this->getDisplay());
+  }
+
+  public function testNetworkErrorFailsSilently(): void {
+    $guzzle_client = $this->prophet->prophesize(Client::class);
+    $guzzle_client->get('https://api.github.com/repos/acquia/cli/releases')
+      ->willThrow(RequestException::class);
+    $this->command->setUpdateClient($guzzle_client->reveal());
+    $this->application->setVersion('2.8.4.9999s');
     $this->executeCommand([], []);
     self::assertEquals(0, $this->getStatusCode());
     self::assertStringNotContainsString('Acquia CLI 2.8.5 is available', $this->getDisplay());
