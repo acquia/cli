af801e5c89e10316baa1c6776a26cc2bb8cee9e5
Dane Powell <git@danepowell.com>
2023-06-05 13:56:22 -0700

    remove cache buster

--- tests/phpunit/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-use Symfony\Component\Filesystem\Filesystem;
-
-// ensure a fresh cache when debug mode is disabled
-(new Filesystem())->remove(__DIR__ . '/../../var/cache/dev');
