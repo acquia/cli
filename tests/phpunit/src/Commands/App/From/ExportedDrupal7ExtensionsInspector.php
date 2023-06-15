fc6d16e5a4fa919150fe22397b0a12917e890a04
Wim Leers <wim.leers@acquia.com>
2023-06-15 13:41:24 +0200

    Adjust for stricter `phpcs` configuration.

--- tests/phpunit/src/Commands/App/From/ExportedDrupal7ExtensionsInspector.php
+++ tests/phpunit/src/Commands/App/From/ExportedDrupal7ExtensionsInspector.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Acquia\Cli\Tests\Commands\App\From;
 
@@ -8,11 +8,6 @@ use Acquia\Cli\Command\App\From\JsonResourceParserTrait;
 use Acquia\Cli\Command\App\From\SourceSite\Drupal7Extension;
 use Acquia\Cli\Command\App\From\SourceSite\SiteInspectorBase;
 
-/**
- * Class ExportedDrupal7ExtensionsInspector.
- *
- * @internal
- */
 class ExportedDrupal7ExtensionsInspector extends SiteInspectorBase {
 
   use JsonResourceParserTrait;
@@ -21,7 +16,7 @@ class ExportedDrupal7ExtensionsInspector extends SiteInspectorBase {
    * ExportedDrupal7ExtensionsInspector constructor.
    *
    * @param \Acquia\Cli\Command\App\From\SourceSite\Drupal7Extension[] $extensions
-   *   An array of Drupal 7 extensions.
+   *   An array of extensions.
    */
   protected function __construct(
     protected array $extensions
@@ -30,16 +25,12 @@ class ExportedDrupal7ExtensionsInspector extends SiteInspectorBase {
   /**
    * Creates a new ExportedDrupal7ExtensionsInspector.
    *
-   * @param $extensions_resource
+   * @param resource $extensions_resource
    *   A resource containing a list of Drupal 7 extension information.
-   *
    * @return static
    *   A new instance of this class.
-   *
-   * @throws \JsonException
-   *   Thrown if the given resource contains malformed JSON.
    */
-  public static function createFromResource($extensions_resource) {
+  public static function createFromResource($extensions_resource): static {
     assert(is_resource($extensions_resource));
     return new static(static::parseExtensionsFromResource($extensions_resource));
   }
@@ -51,16 +42,10 @@ class ExportedDrupal7ExtensionsInspector extends SiteInspectorBase {
     return $this->extensions;
   }
 
-  /**
-   * {@inheritDoc}
-   */
   public function getPublicFilePath(): string {
     return 'sites/default/files';
   }
 
-  /**
-   * {@inheritDoc}
-   */
   public function getPrivateFilePath(): ?string {
     return NULL;
   }
@@ -68,15 +53,12 @@ class ExportedDrupal7ExtensionsInspector extends SiteInspectorBase {
   /**
    * Reads an extensions resource into extensions objects.
    *
-   * @param $extensions_resource
+   * @param resource $extensions_resource
    *   A serialized extensions resource from which to parse extensions.
-   *
    * @return \Acquia\Cli\Command\App\From\SourceSite\Drupal7Extension[]
    *   An array of extensions.
-   *
-   * @throws \JsonException
    */
-  protected static function parseExtensionsFromResource($extensions_resource) : array {
+  protected static function parseExtensionsFromResource($extensions_resource): array {
     return array_map(function (array $extension) {
       $extension['status'] = $extension['enabled'];
       return Drupal7Extension::createFromStdClass((object) $extension);
