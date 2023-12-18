c90e817b4145ede32c7cdc3288d210c3bf0bbd91
Josh Waihi <josh.waihi@acquia.com>
2023-12-18 14:26:58 +1300

    Allow other descriptor formats to be used to render docs.

--- src/Command/Self/MakeDocsCommand.php
+++ src/Command/Self/MakeDocsCommand.php
@@ -6,6 +6,7 @@ namespace Acquia\Cli\Command\Self;
 
 use Acquia\Cli\Attribute\RequireAuth;
 use Acquia\Cli\Command\CommandBase;
+use Composer\Console\Input\InputOption;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\DescriptorHelper;
@@ -16,11 +17,15 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'self:make-docs', description: 'Generate documentation for all ACLI commands', hidden: TRUE)]
 final class MakeDocsCommand extends CommandBase {
 
+  protected function configure(): void {
+    $this->addOption('format', 'f', InputOption::VALUE_OPTIONAL, 'The format to describe the docs in.', 'rst');
+  }
+
   protected function execute(InputInterface $input, OutputInterface $output): int {
     $helper = new DescriptorHelper();
 
     $helper->describe($output, $this->getApplication(), [
-      'format' => 'rst',
+      'format' => $input->getOption('format'),
     ]);
 
     return Command::SUCCESS;
