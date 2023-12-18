0e06f53d33bcfd5934b9510c0a7f286672dfa79f
Josh Waihi <josh@joshwaihi.com>
2023-12-19 10:47:15 +1300

    Allow other descriptor formats to be used to render docs. (#1655)
    * Allow other descriptor formats to be used to render docs.
    
    * Fixed use of incorrect namespace.

--- src/Command/Self/MakeDocsCommand.php
+++ src/Command/Self/MakeDocsCommand.php
@@ -10,17 +10,22 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\DescriptorHelper;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[RequireAuth]
 #[AsCommand(name: 'self:make-docs', description: 'Generate documentation for all ACLI commands', hidden: TRUE)]
 final class MakeDocsCommand extends CommandBase {
 
+  protected function configure(): void {
+    $this->addOption('format', 'f', InputOption::VALUE_OPTIONAL, 'The format to describe the docs in.', 'rst');
+  }
+
   protected function execute(InputInterface $input, OutputInterface $output): int {
     $helper = new DescriptorHelper();
 
     $helper->describe($output, $this->getApplication(), [
-      'format' => 'rst',
+      'format' => $input->getOption('format'),
     ]);
 
     return Command::SUCCESS;
