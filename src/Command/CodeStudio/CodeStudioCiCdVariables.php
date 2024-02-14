75a720c0c9beb22b78ce43bb3516cd3a5346e130
Akash Karangale <akash.karangale@acquia.com>
2024-02-14 16:28:28 +0530

    GL-2039: Updated getlist for pipeline migrate command to use drupal project as default.

--- src/Command/CodeStudio/CodeStudioCiCdVariables.php
+++ src/Command/CodeStudio/CodeStudioCiCdVariables.php
@@ -10,69 +10,13 @@ class CodeStudioCiCdVariables {
    * @return array<mixed>
    */
   public static function getList(): array {
-    return array_column(self::getDefaults(), 'key');
+    //getlist is being utilised in pipeline-migrate command. By default command is supporting drupal project but going forward need to support both drupal and nodejs project.
+    return array_column(self::getDefaultsForPhp(), 'key');
   }
 
   /**
-   * @return array<mixed>
-   */
-  public static function getDefaults(?string $cloudApplicationUuid = NULL, ?string $cloudKey = NULL, ?string $cloudSecret = NULL, ?string $projectAccessTokenName = NULL, ?string $projectAccessToken = NULL, ?string $phpVersion = NULL, ?string $nodeVersion = NULL): array {
-    return [
-      [
-        'key' => 'ACQUIA_APPLICATION_UUID',
-        'masked' => TRUE,
-        'protected' => FALSE,
-        'value' => $cloudApplicationUuid,
-        'variable_type' => 'env_var',
-      ],
-      [
-        'key' => 'ACQUIA_CLOUD_API_TOKEN_KEY',
-        'masked' => TRUE,
-        'protected' => FALSE,
-        'value' => $cloudKey,
-        'variable_type' => 'env_var',
-      ],
-      [
-        'key' => 'ACQUIA_CLOUD_API_TOKEN_SECRET',
-        'masked' => TRUE,
-        'protected' => FALSE,
-        'value' => $cloudSecret,
-        'variable_type' => 'env_var',
-      ],
-      [
-        'key' => 'ACQUIA_GLAB_TOKEN_NAME',
-        'masked' => TRUE,
-        'protected' => FALSE,
-        'value' => $projectAccessTokenName,
-        'variable_type' => 'env_var',
-      ],
-      [
-        'key' => 'ACQUIA_GLAB_TOKEN_SECRET',
-        'masked' => TRUE,
-        'protected' => FALSE,
-        'value' => $projectAccessToken,
-        'variable_type' => 'env_var',
-      ],
-      [
-        'key' => 'PHP_VERSION',
-        'masked' => FALSE,
-        'protected' => FALSE,
-        'value' => $phpVersion,
-        'variable_type' => 'env_var',
-      ],
-      [
-        'key' => 'NODE_VERSION',
-        'masked' => FALSE,
-        'protected' => FALSE,
-        'value' => $nodeVersion,
-        'variable_type' => 'env_var',
-      ],
-    ];
-  }
-
-   /**
-   * @return array<mixed>
-   */
+  * @return array<mixed>
+  */
   public static function getDefaultsForNode(?string $cloudApplicationUuid = NULL, ?string $cloudKey = NULL, ?string $cloudSecret = NULL, ?string $projectAccessTokenName = NULL, ?string $projectAccessToken = NULL, ?string $nodeVersion = NULL): array {
     return [
       [
@@ -120,9 +64,9 @@ class CodeStudioCiCdVariables {
     ];
   }
 
-   /**
-   * @return array<mixed>
-   */
+  /**
+  * @return array<mixed>
+  */
   public static function getDefaultsForPhp(?string $cloudApplicationUuid = NULL, ?string $cloudKey = NULL, ?string $cloudSecret = NULL, ?string $projectAccessTokenName = NULL, ?string $projectAccessToken = NULL, ?string $phpVersion = NULL): array {
     return [
       [
