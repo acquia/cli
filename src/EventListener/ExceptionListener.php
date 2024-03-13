807b79e422dd9e8434bc38cc1dc49faf45fd42bb
Dane Powell <git@danepowell.com>
2024-03-13 13:36:20 -0700

    CLI-1292: Catch Cloud API type errors

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -79,6 +79,10 @@ class ExceptionListener {
       }
     }
 
+    if ($error instanceof \TypeError && str_contains($error->getMessage(), 'AcquiaCloudApi\Response')) {
+      $newErrorMessage = 'Cloud API returned an unexpected data type; this could indicate a problem with your Cloud application.';
+    }
+
     $this->helpMessages[] = "You can find Acquia CLI documentation at https://docs.acquia.com/acquia-cli/";
     $this->writeUpdateHelp($event);
     $this->writeSupportTicketHelp($event);
