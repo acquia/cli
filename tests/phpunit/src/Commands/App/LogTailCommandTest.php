ccde5450152b96d9e407d949bbac5d1463f16dfe
Dane Powell <git@danepowell.com>
2024-04-25 15:48:20 -0700

    kill mutant

--- tests/phpunit/src/Commands/App/LogTailCommandTest.php
+++ tests/phpunit/src/Commands/App/LogTailCommandTest.php
@@ -95,6 +95,38 @@ class LogTailCommandTest extends CommandTestBase {
     $this->assertStringContainsString('Drupal request', $output);
   }
 
+  public function testLogTailNode(): void {
+    $applications = $this->mockRequest('getApplications');
+    $application = $this->mockRequest('getApplicationByUuid', $applications[self::$INPUT_DEFAULT_CHOICE]->uuid);
+    $tamper = function (&$responses): void {
+      foreach ($responses as $response) {
+        $response->type = 'node';
+      }
+    };
+    $this->mockRequest('getApplicationEnvironments', $application->uuid, NULL, NULL, $tamper);
+    $this->mockLogStreamRequest();
+    $this->executeCommand([], [
+      // Would you like Acquia CLI to search for a Cloud application that matches your local git config?
+      'n',
+      // Select the application.
+      0,
+      // Would you like to link the project at ... ?
+      'y',
+      // Select environment.
+      0,
+      // Select log.
+      0,
+    ]);
+
+    // Assert.
+    $output = $this->getDisplay();
+    $this->assertStringContainsString('Select a Cloud Platform application:', $output);
+    $this->assertStringContainsString('[0] Sample application 1', $output);
+    $this->assertStringContainsString('[1] Sample application 2', $output);
+    $this->assertStringContainsString('Apache request', $output);
+    $this->assertStringContainsString('Drupal request', $output);
+  }
+
   private function mockLogStreamRequest(): void {
     $response = $this->getMockResponseFromSpec('/environments/{environmentId}/logstream',
       'get', '200');
