f0cfaa044d362eedc28101a6208f4a0eb6964f8d
Dane Powell <git@danepowell.com>
2024-05-07 11:32:31 -0700

    Kill mutants

--- tests/phpunit/src/Commands/Env/EnvCertCreateCommandTest.php
+++ tests/phpunit/src/Commands/Env/EnvCertCreateCommandTest.php
@@ -14,13 +14,10 @@ class EnvCertCreateCommandTest extends CommandTestBase {
     return $this->injectCommand(EnvCertCreateCommand::class);
   }
 
-  /**
-   * @group brokenProphecy
-   */
   public function testCreateCert(): void {
-    $applicationsResponse = $this->mockApplicationsRequest();
-    $this->mockApplicationRequest();
-    $environmentsResponse = $this->mockEnvironmentsRequest($applicationsResponse);
+    $applications = $this->mockRequest('getApplications');
+    $application = $this->mockRequest('getApplicationByUuid', $applications[self::$INPUT_DEFAULT_CHOICE]->uuid);
+    $environments = $this->mockRequest('getApplicationEnvironments', $application->uuid);
     $localMachineHelper = $this->mockLocalMachineHelper();
     $certContents = 'cert-contents';
     $keyContents = 'key-contents';
@@ -43,7 +40,64 @@ class EnvCertCreateCommandTest extends CommandTestBase {
         'private_key' => $keyContents,
         ],
     ];
-    $this->clientProphecy->request('post', "/environments/{$environmentsResponse->{'_embedded'}->items[0]->id}/ssl/certificates", $options)
+    $this->clientProphecy->request('post', "/environments/{$environments[1]->id}/ssl/certificates", $options)
+      ->willReturn($sslResponse->{'Site is being imported'}->value)
+      ->shouldBeCalled();
+    $this->mockNotificationResponseFromObject($sslResponse->{'Site is being imported'}->value);
+
+    $this->executeCommand(
+      [
+        '--csr-id' => $csrId,
+        '--label' => $label,
+        '--legacy' => FALSE,
+        'certificate' => $certName,
+        'private-key' => $keyName,
+      ],
+      [
+        // Would you like Acquia CLI to search for a Cloud application that matches your local git config?'.
+        'n',
+        // Select a Cloud Platform application: [Sample application 1]:
+        0,
+        'n',
+        1,
+        '',
+      ]
+    );
+
+  }
+
+  public function testCreateCertNode(): void {
+    $applications = $this->mockRequest('getApplications');
+    $application = $this->mockRequest('getApplicationByUuid', $applications[self::$INPUT_DEFAULT_CHOICE]->uuid);
+    $tamper = function ($responses): void {
+      foreach ($responses as $response) {
+        $response->type = 'node';
+      }
+    };
+    $environments = $this->mockRequest('getApplicationEnvironments', $application->uuid, NULL, NULL, $tamper);
+    $localMachineHelper = $this->mockLocalMachineHelper();
+    $certContents = 'cert-contents';
+    $keyContents = 'key-contents';
+    $certName = 'cert.pem';
+    $keyName = 'key.pem';
+    $label = 'My certificate';
+    $csrId = 123;
+    $localMachineHelper->readFile($certName)->willReturn($certContents)->shouldBeCalled();
+    $localMachineHelper->readFile($keyName)->willReturn($keyContents)->shouldBeCalled();
+
+    $sslResponse = $this->getMockResponseFromSpec('/environments/{environmentId}/ssl/certificates',
+      'post', '202');
+    $options = [
+      'json' => [
+        'ca_certificates' => NULL,
+        'certificate' => $certContents,
+        'csr_id' => $csrId,
+        'label' => $label,
+        'legacy' => FALSE,
+        'private_key' => $keyContents,
+      ],
+    ];
+    $this->clientProphecy->request('post', "/environments/{$environments[0]->id}/ssl/certificates", $options)
       ->willReturn($sslResponse->{'Site is being imported'}->value)
       ->shouldBeCalled();
     $this->mockNotificationResponseFromObject($sslResponse->{'Site is being imported'}->value);
