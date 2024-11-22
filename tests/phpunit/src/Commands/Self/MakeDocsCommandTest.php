2f6a179e8129cd171cd1ca542d05b9faa5fa22d8
Dane Powell <git@danepowell.com>
2024-11-21 16:04:42 -0800

    kill mutant

--- tests/phpunit/src/Commands/Self/MakeDocsCommandTest.php
+++ tests/phpunit/src/Commands/Self/MakeDocsCommandTest.php
@@ -7,6 +7,7 @@ namespace Acquia\Cli\Tests\Commands\Self;
 use Acquia\Cli\Command\CommandBase;
 use Acquia\Cli\Command\Self\MakeDocsCommand;
 use Acquia\Cli\Tests\CommandTestBase;
+use org\bovigo\vfs\vfsStream;
 
 /**
  * @property \Acquia\Cli\Command\Self\MakeDocsCommand $command
@@ -25,5 +26,12 @@ class MakeDocsCommandTest extends CommandTestBase
         $this->assertStringContainsString('Console Tool', $output);
         $this->assertStringContainsString('============', $output);
         $this->assertStringContainsString('- `help`_', $output);
+    }
+
+    public function testMakeDocsCommandDump(): void
+    {
+        $vfs = vfsStream::setup('root');
+        $this->executeCommand(['--dump' => $vfs->url()]);
+        $this->assertStringContainsString('The completion command dumps', $vfs->getChild('completion.json')->getContent());
     }
 }
