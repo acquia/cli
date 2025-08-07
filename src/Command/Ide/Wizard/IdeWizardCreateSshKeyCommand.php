1bdcd370d532868182eaba42d4f9e774a4dabd49
Jigar Shah <jigar.shah@acquia.com>
2025-08-07 10:23:35 +0530

    CLI-1593: acli ide:wizard:ssh-key:create-upload codebase compatiblity.

--- src/Command/Ide/Wizard/IdeWizardCreateSshKeyCommand.php
+++ src/Command/Ide/Wizard/IdeWizardCreateSshKeyCommand.php
@@ -93,7 +93,11 @@ final class IdeWizardCreateSshKeyCommand extends IdeWizardCommandBase
 
         // Wait for the key to register on the Cloud Platform.
         if ($keyWasUploaded) {
-            if ($this->input->isInteractive() && !$this->promptWaitForSsh($this->io)) {
+            if ($this->getCodebaseUuid()) {
+                $output->writeln('<info>SSH key has been successfully uploaded to the Cloud Platform.</info>');
+                $this->io->note("It may take an hour or more before the SSH key is installed on all of your servers. Create a Support ticket for further assistance.");
+                return Command::SUCCESS;
+            } elseif ($this->input->isInteractive() && !$this->promptWaitForSsh($this->io)) {
                 $this->io->success('Your SSH key has been successfully uploaded to the Cloud Platform.');
                 return Command::SUCCESS;
             }
