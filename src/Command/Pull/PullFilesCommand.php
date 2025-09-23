d0e35e459151dfc8df7530a17517f0dec9606da2
jigar-shah-acquia <jigar.shah@acquia.com>
2025-09-24 02:24:28 +0530

    IDE-4565: acli pull-files not showing the Prod environment option on Cloud IDE | bca.prod | 01204920. (#1906)
    * IDE-4565: Solved environment issue in acli pull-files.
    
    * IDE-4565: Solved environment issue in acli pull-files.
    
    ---------
    
    Co-authored-by: Acquia CI/CD <no-reply@acquia.com>

--- src/Command/Pull/PullFilesCommand.php
+++ src/Command/Pull/PullFilesCommand.php
@@ -26,7 +26,7 @@ final class PullFilesCommand extends PullCommandBase
     {
         $this->setDirAndRequireProjectCwd($input);
 
-        $sourceEnvironment = $this->determineEnvironment($input, $output);
+        $sourceEnvironment = $this->determineEnvironment($input, $output, true);
 
         $this->pullFiles($input, $output, $sourceEnvironment);
 
