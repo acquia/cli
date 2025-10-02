988435bbe9dba5b8be080f15c9a62b3a22d83381
Adam Balsam <balsama@users.noreply.github.com>
2025-10-02 13:38:47 -0400

    Remove Next ACMS from list of new applications (#1911)
    * Remove Next ACMS from list of new applications
    
    * Assert option is no longer available
    
    * Remove tests for next acms
    
    * Add explicit assertion that Next ACMS is not an option.
    
    * Remove orphaned code paths

--- tests/phpunit/src/Commands/App/NewCommandTest.php
+++ tests/phpunit/src/Commands/App/NewCommandTest.php
@@ -46,17 +46,6 @@ class NewCommandTest extends CommandTestBase
     }
 
     /**
-     * @return array<mixed>
-     */
-    public static function provideTestNewNextJsAppCommand(): array
-    {
-        return [
-            [['acquia_next_acms' => 'acquia/next-acms']],
-            [['acquia_next_acms' => 'acquia/next-acms'], 'test-dir'],
-        ];
-    }
-
-    /**
      * @dataProvider provideTestNewDrupalCommand
      */
     public function testNewDrupalCommand(array $package, string $directory = 'drupal'): void
@@ -92,85 +81,11 @@ class NewCommandTest extends CommandTestBase
         $output = $this->getDisplay();
         $this->assertStringContainsString('Acquia recommends most customers use acquia/drupal-recommended-project to setup a Drupal project', $output);
         $this->assertStringContainsString('acquia/drupal-cms-project is Drupal CMS scaffolded to work with Acquia hosting.', $output);
+        $this->assertStringNotContainsString('acquia/next-acms is a starter template for building a headless site powered by Acquia CMS and Next.js.', $output);
         $this->assertStringContainsString('Choose a starting project', $output);
         $this->assertStringContainsString($project, $output);
         $this->assertTrue($mockFileSystem->isAbsolutePath($this->newProjectDir), 'Directory path is not absolute');
         $this->assertStringContainsString('New 💧 Drupal project created in ' . $this->newProjectDir, $output);
-    }
-
-    /**
-     * @dataProvider provideTestNewNextJsAppCommand
-     */
-    public function testNewNextJSAppCommand(array $package, string $directory = 'nextjs'): void
-    {
-        $this->newProjectDir = Path::makeAbsolute($directory, $this->projectDir);
-        $projectKey = array_keys($package)[0];
-        $project = $package[$projectKey];
-
-        $process = $this->prophet->prophesize(Process::class);
-        $process->isSuccessful()->willReturn(true);
-        $process->getExitCode()->willReturn(0);
-
-        $localMachineHelper = $this->mockLocalMachineHelper();
-
-        $mockFileSystem = $this->mockGetFilesystem($localMachineHelper);
-
-        $localMachineHelper->checkRequiredBinariesExist(["node"])
-            ->shouldBeCalled();
-        $this->mockExecuteNpxCreate($this->newProjectDir, $localMachineHelper, $process);
-        $localMachineHelper->checkRequiredBinariesExist(["git"])
-            ->shouldBeCalled();
-        $this->mockExecuteGitInit($localMachineHelper, $this->newProjectDir, $process);
-        $this->mockExecuteGitAdd($localMachineHelper, $this->newProjectDir, $process);
-        $this->mockExecuteGitCommit($localMachineHelper, $this->newProjectDir, $process);
-
-        $inputs = [
-            // Choose a starting project.
-            $project,
-        ];
-        $this->executeCommand([
-            'directory' => $directory,
-        ], $inputs);
-
-        $output = $this->getDisplay();
-        $this->assertStringContainsString('acquia/next-acms is a starter template for building a headless site', $output);
-        $this->assertStringContainsString('Choose a starting project', $output);
-        $this->assertStringContainsString($project, $output);
-        $this->assertTrue($mockFileSystem->isAbsolutePath($this->newProjectDir), 'Directory path is not absolute');
-        $this->assertStringContainsString('New Next.js project created in ' . $this->newProjectDir, $output);
-    }
-
-    public function testProjectTemplateOption(): void
-    {
-        $this->newProjectDir = Path::makeAbsolute('nextjs', $this->projectDir);
-
-        $process = $this->prophet->prophesize(Process::class);
-        $process->isSuccessful()->willReturn(true);
-        $process->getExitCode()->willReturn(0);
-
-        $localMachineHelper = $this->mockLocalMachineHelper();
-
-        $mockFileSystem = $this->mockGetFilesystem($localMachineHelper);
-
-        $localMachineHelper->checkRequiredBinariesExist(["node"])
-            ->shouldBeCalled();
-        $this->mockExecuteNpxCreate($this->newProjectDir, $localMachineHelper, $process);
-        $localMachineHelper->checkRequiredBinariesExist(["git"])
-            ->shouldBeCalled();
-        $this->mockExecuteGitInit($localMachineHelper, $this->newProjectDir, $process);
-        $this->mockExecuteGitAdd($localMachineHelper, $this->newProjectDir, $process);
-        $this->mockExecuteGitCommit($localMachineHelper, $this->newProjectDir, $process);
-
-        $this->executeCommand([
-            '--template' => 'acquia_next_acms',
-            'directory' => 'nextjs',
-        ]);
-
-        $output = $this->getDisplay();
-        $this->assertStringContainsString('Acquia recommends most customers use acquia/drupal-recommended-project to setup a Drupal project', $output);
-        $this->assertStringContainsString('acquia/next-acms', $output);
-        $this->assertTrue($mockFileSystem->isAbsolutePath($this->newProjectDir), 'Directory path is not absolute');
-        $this->assertStringContainsString('New Next.js project created in ' . $this->newProjectDir, $output);
     }
 
     protected function mockExecuteComposerCreate(
@@ -185,24 +100,6 @@ class NewCommandTest extends CommandTestBase
             $project,
             $projectDir,
             '--no-interaction',
-        ];
-        $localMachineHelper
-            ->execute($command)
-            ->willReturn($process->reveal())
-            ->shouldBeCalled();
-    }
-
-    protected function mockExecuteNpxCreate(
-        string $projectDir,
-        ObjectProphecy $localMachineHelper,
-        ObjectProphecy $process,
-    ): void {
-        $command = [
-            'npx',
-            'create-next-app',
-            '-e',
-            'https://github.com/acquia/next-acms/tree/main/starters/basic-starter',
-            $projectDir,
         ];
         $localMachineHelper
             ->execute($command)
