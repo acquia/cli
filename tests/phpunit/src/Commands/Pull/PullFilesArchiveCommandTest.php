064c2d46ab1472573a25c48edefefdb24d72e41f
Matthew Grasmick <matt.grasmick@gmail.com>
2026-08-17 16:03:33 -0400

    Add pull:files-archive command
    Adds a pull:files-archive command that copies Drupal public files from a
    Cloud Platform environment by streaming a gzipped tarball over SSH and
    extracting it locally. Unlike pull:files, it does not require a local
    rsync binary (only ssh and tar), which makes it usable on Windows and in
    minimal CI containers, and it issues a single fixed remote command with
    straightforward failure modes: the download process exit code is the
    remote tar's, and extraction is a separate, separately-reported step.

---
+++ tests/phpunit/src/Commands/Pull/PullFilesArchiveCommandTest.php
@@ -0,0 +1,167 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acquia\Cli\Tests\Commands\Pull;
+
+use Acquia\Cli\Command\CommandBase;
+use Acquia\Cli\Command\Pull\PullFilesArchiveCommand;
+use Acquia\Cli\Exception\AcquiaCliException;
+use Acquia\Cli\Helpers\LocalMachineHelper;
+use GuzzleHttp\Client;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class PullFilesArchiveCommandTest extends PullCommandTestBase
+{
+    protected function createCommand(): CommandBase
+    {
+        $this->httpClientProphecy = $this->prophet->prophesize(Client::class);
+
+        return new PullFilesArchiveCommand(
+            $this->localMachineHelper,
+            $this->datastoreCloud,
+            $this->datastoreAcli,
+            $this->cloudCredentials,
+            $this->telemetryHelper,
+            $this->acliRepoRoot,
+            $this->clientServiceProphecy->reveal(),
+            $this->sshHelper,
+            $this->sshDir,
+            $this->logger,
+            $this->selfUpdateManager,
+            $this->httpClientProphecy->reveal()
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testPullFilesArchiveCloud(): void
+    {
+        $applicationsResponse = $this->mockApplicationsRequest();
+        $this->mockApplicationRequest();
+        $environmentsResponse = $this->mockEnvironmentsRequest($applicationsResponse);
+        $selectedEnvironment = $environmentsResponse->_embedded->items[0];
+        $sshHelper = $this->mockSshHelper();
+        $this->mockGetCloudSites($sshHelper, $selectedEnvironment);
+        $localMachineHelper = $this->mockLocalMachineHelper();
+        $this->mockGetFilesystem($localMachineHelper);
+        $parts = explode('.', $selectedEnvironment->ssh_url);
+        $sitegroup = reset($parts);
+        $this->mockExecuteFilesArchiveDownload(
+            $localMachineHelper,
+            $selectedEnvironment,
+            '/mnt/files/' . $sitegroup . '.' . $selectedEnvironment->name . '/sites/default/files',
+            $this->projectDir . '/docroot/sites/default/files'
+        );
+
+        $this->command->sshHelper = $sshHelper->reveal();
+
+        $inputs = [
+            // Would you like Acquia CLI to search for a Cloud application that matches your local git config?
+            'n',
+            // Select a Cloud Platform application:
+            0,
+            // Would you like to link the project at ... ?
+            'n',
+            // Choose an Acquia environment:
+            0,
+            // Choose site from which to copy files:
+            0,
+        ];
+
+        $this->executeCommand([], $inputs);
+
+        $output = $this->getDisplay();
+
+        $this->assertStringContainsString('Select a Cloud Platform application', $output);
+        $this->assertStringContainsString('[0] Sample application 1', $output);
+        $this->assertStringContainsString('Choose a Cloud Platform environment', $output);
+        $this->assertStringContainsString('[0] Dev, dev (vcs: master)', $output);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testPullFilesArchiveCloudDownloadFails(): void
+    {
+        $applicationsResponse = $this->mockApplicationsRequest();
+        $this->mockApplicationRequest();
+        $environmentsResponse = $this->mockEnvironmentsRequest($applicationsResponse);
+        $selectedEnvironment = $environmentsResponse->_embedded->items[0];
+        $sshHelper = $this->mockSshHelper();
+        $this->mockGetCloudSites($sshHelper, $selectedEnvironment);
+        $localMachineHelper = $this->mockLocalMachineHelper();
+        $this->mockGetFilesystem($localMachineHelper);
+        $localMachineHelper->checkRequiredBinariesExist(['ssh', 'tar'])
+            ->shouldBeCalled();
+        $failedProcess = $this->mockProcess(false);
+        $localMachineHelper->executeFromCmd(
+            Argument::containingString('"${:REMOTE_COMMAND}"'),
+            Argument::type('callable'),
+            null,
+            false,
+            null,
+            Argument::type('array')
+        )
+            ->willReturn($failedProcess->reveal())
+            ->shouldBeCalled();
+
+        $this->command->sshHelper = $sshHelper->reveal();
+
+        $inputs = [
+            // Would you like Acquia CLI to search for a Cloud application that matches your local git config?
+            'n',
+            // Select a Cloud Platform application:
+            0,
+            // Would you like to link the project at ... ?
+            'n',
+            // Choose an Acquia environment:
+            0,
+            // Choose site from which to copy files:
+            0,
+        ];
+
+        $this->expectException(AcquiaCliException::class);
+        $this->expectExceptionMessage('Unable to download files.');
+        $this->executeCommand([], $inputs);
+    }
+
+    protected function mockExecuteFilesArchiveDownload(
+        LocalMachineHelper|ObjectProphecy $localMachineHelper,
+        mixed $environment,
+        string $sourceDir,
+        string $destinationDir
+    ): void {
+        $process = $this->mockProcess();
+        $localMachineHelper->checkRequiredBinariesExist(['ssh', 'tar'])
+            ->shouldBeCalled();
+        $localMachineHelper->executeFromCmd(
+            Argument::containingString('ssh -o StrictHostKeyChecking=accept-new "${:SSH_URL}" "${:REMOTE_COMMAND}"'),
+            Argument::type('callable'),
+            null,
+            false,
+            null,
+            Argument::that(static function (array $env) use ($environment, $sourceDir): bool {
+                return $env['SSH_URL'] === $environment->ssh_url
+                    && $env['REMOTE_COMMAND'] === "tar -C $sourceDir -czf - .";
+            })
+        )
+            ->willReturn($process->reveal())
+            ->shouldBeCalled();
+        $localMachineHelper->execute(
+            Argument::that(static function (array $command) use ($destinationDir): bool {
+                return $command[0] === 'tar'
+                    && $command[1] === '-xzf'
+                    && $command[3] === '-C'
+                    && $command[4] === $destinationDir;
+            }),
+            Argument::type('callable'),
+            null,
+            false
+        )
+            ->willReturn($process->reveal())
+            ->shouldBeCalled();
+    }
+}
