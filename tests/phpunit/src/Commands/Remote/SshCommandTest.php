8312a218c1f28f2c013b8a3b851c60eb1a8a9a4c
Dane Powell <git@danepowell.com>
2026-09-09 19:20:28 -0700

    kill mutant

--- tests/phpunit/src/Commands/Remote/SshCommandTest.php
+++ tests/phpunit/src/Commands/Remote/SshCommandTest.php
@@ -45,6 +45,41 @@ class SshCommandTest extends SshCommandTestBase
         $this->getDisplay();
     }
 
+    public function testRemoteSshCommandAllowsProductionEnvironment(): void
+    {
+        $this->mockGetEnvironment();
+        [$process, $localMachineHelper] = $this->mockForExecuteCommand();
+        $localMachineHelper->checkRequiredBinariesExist(['ssh'])
+            ->shouldBeCalled();
+        $sshCommand = [
+            'ssh',
+            'site.prod@siteprod.ssh.hosted.acquia-sites.com',
+            '-t',
+            '-o StrictHostKeyChecking=accept-new',
+            '-o AddressFamily inet',
+            '-o LogLevel=ERROR',
+            'cd /var/www/html/site.prod; exec $SHELL -l',
+        ];
+        $localMachineHelper
+            ->execute($sshCommand, Argument::type('callable'), null, true, null, null)
+            ->willReturn($process->reveal())
+            ->shouldBeCalled();
+
+        $this->command->sshHelper = new SshHelper($this->output, $localMachineHelper->reveal(), $this->logger);
+        $this->executeCommand(['ssh_command' => []], [
+            // Would you like Acquia CLI to search for a Cloud application that matches your local git config?
+            'n',
+            // Select a Cloud Platform application:
+            self::$INPUT_DEFAULT_CHOICE,
+            // Would you like to link the project at ... ?
+            'n',
+            // Choose a Cloud Platform environment (index 1 = prod):
+            1,
+        ]);
+
+        $this->getDisplay();
+    }
+
     public function testRemoteSshCommandWithEnvUuid(): void
     {
         $this->mockRequest('getEnvironment', '24-a47ac10b-58cc-4372-a567-0e02b2c3d470');
